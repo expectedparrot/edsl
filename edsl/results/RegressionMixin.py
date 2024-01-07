98142aff7a132970b4522d6143563811484d086f
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-01-06 20:21:45 -0500

    a little cleaning in results/RegressionMixin

--- edsl/results/RegressionMixin.py
+++ edsl/results/RegressionMixin.py
@@ -1,39 +1,28 @@
-import pandas as pd
-import statsmodels.api as sm
 import re
-import numpy as np
+import statsmodels.api as sm
+from typing import List
 
 
-def extract_variable_names(formula):
-    # Use regular expressions to find all variable names within the formula
+def extract_variable_names(formula: str) -> List[str]:
+    """
+    Extracts the variable names from a formula
+    TODO: improve the way we handle the case if there is a variable named 'C'
+    """
     variable_names = re.findall(r"\b\w+\b", formula)
-    # what do we do if there is a variable named 'C'?
     return [variable_name for variable_name in variable_names if variable_name != "C"]
 
 
 class RegressionMixin:
-    def regression(self, formula):
-        """
-        This runs a linear regression
-        https://www.statsmodels.org/stable/index.html
-        formula = 'y ~ x1 + x2 + x3 + C(x4)'
-        """
-
+    def regression(
+        self, formula: str
+    ) -> sm.regression.linear_model.RegressionResultsWrapper:
+        """Runs a linear regression"""
         variable_names = extract_variable_names(formula)
-
         df = self.select(*variable_names).to_pandas()
-
         # get rid of the headers
         column_mapping = {col: col.split(".")[-1] for col in df.columns}
         # Rename the columns using the dictionary
         df.rename(columns=column_mapping, inplace=True)
-
         model = sm.OLS.from_formula(formula, data=df)
         regression_result = model.fit()
-
         return regression_result
-
-
-if __name__ == "__main__":
-    formula = "y ~ x1 + x2 + C(customer_service)"
-    print(extract_variable_names_with_indicator(formula))
