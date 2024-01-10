bb60d96afa73659785de6b3963b1f12ef46e4851
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-01-10 18:35:32 -0500

    work on QuestionYesNo

--- edsl/questions/derived/QuestionYesNo.py
+++ edsl/questions/derived/QuestionYesNo.py
@@ -1,16 +1,9 @@
-from typing import Type
-from edsl.exceptions import (
-    QuestionCreationValidationError,
-    QuestionAnswerValidationError,
-)
-from edsl.questions import Settings, QuestionData, AnswerData
-from edsl.questions.QuestionMultipleChoice import QuestionMultipleChoice
-
 from edsl.questions.descriptors import QuestionOptionsDescriptor
+from edsl.questions.QuestionMultipleChoice import QuestionMultipleChoice
 
 
 class QuestionYesNo(QuestionMultipleChoice):
-    """Same as a QuestionCheckBox, but with question_options=["yes","no"]"""
+    """Same as a QuestionMultipleChoice, but with question_options=["Yes","No"]"""
 
     question_type = "yes_no"
     question_options = QuestionOptionsDescriptor(num_choices=2)
@@ -31,3 +24,29 @@ class QuestionYesNo(QuestionMultipleChoice):
             instructions=instructions,
         )
         self.question_options = question_options
+
+    ################
+    # Example
+    ################
+    @classmethod
+    def example(cls):
+        return cls(
+            question_name="is_it_raining",
+            question_text="Is it raining?",
+            short_names_dict={"Yes": "y", "No": "y"},
+        )
+
+
+def main():
+    from edsl.questions.derived.QuestionYesNo import QuestionYesNo
+
+    q = QuestionYesNo.example()
+    q.question_text
+    q.question_options
+    q.question_name
+    q.short_names_dict
+    q.instructions
+    # validate an answer
+    q.validate_answer({"answer": 0, "comment": "I like custard"})
+    # translate answer code
+    q.translate_answer_code_to_answer(0, {})
