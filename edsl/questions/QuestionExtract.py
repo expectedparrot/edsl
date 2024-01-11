0c6f833fdb41c6eb8d119a98b081317ef065fb7b
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-01-11 14:09:32 -0500

    work on QuestionExtract

--- edsl/questions/QuestionExtract.py
+++ edsl/questions/QuestionExtract.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import random
 import textwrap
-from typing import Any
+from typing import Any, Optional
 from edsl.questions import Question
 from edsl.questions.descriptors import AnswerTemplateDescriptor
 from edsl.scenarios import Scenario
@@ -9,12 +9,26 @@ from edsl.utilities import random_string
 
 
 class QuestionExtract(Question):
+    """
+    This question asks the user to extract values from a string, and return them in a given template.
+
+    Arguments:
+    - `question_name` is the name of the question (string)
+    - `question_text` is the text of the question (string)
+    - `answer_template` is the template for the answer (dictionary mapping strings to strings)
+
+    Optional arguments:
+    - `instructions` are the instructions for the question (string). If not provided, the default instructions are used. To view them, run `QuestionExtract.default_instructions`
+
+    For an example, run `QuestionExtract.example()`
+    """
+
     question_type = "extract"
     answer_template: dict[str, Any] = AnswerTemplateDescriptor()
     default_instructions = textwrap.dedent(
         """\
         You are given the following input: "{{question_text}}".
-        Create an ANSWER should be formatted like this {{ answer_template }},
+        Create an ANSWER should be formatted like this: "{{ answer_template }}",
         and it should have the same keys but values extracted from the input.
         If the value of a key is not present in the input, fill with "null".
         Return a valid JSON formatted like this: 
@@ -28,14 +42,12 @@ class QuestionExtract(Question):
         question_text: str,
         answer_template: dict[str, Any],
         question_name: str,
-        short_names_dict: dict[str, str] = None,
-        instructions: str = None,
+        instructions: Optional[str] = None,
     ):
+        self.question_name = question_name
         self.question_text = question_text
         self.answer_template = answer_template
-        self.question_name = question_name
         self.instructions = instructions or self.default_instructions
-        self.short_names_dict = short_names_dict or dict()
 
     ################
     # Answer methods
