ae9fa89dc38934607497678b9181eead85ffec0a
John Horton <john.joseph.horton@gmail.com>
2024-01-13 22:42:04 -0500

    Deleting unused runners

--- edsl/jobs/JobsRunner.py
+++ edsl/jobs/JobsRunner.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from abc import ABC, abstractmethod
 from edsl.jobs import Jobs
 from edsl.results import Results
--- integration/test_memory.py
+++ integration/test_memory.py
@@ -2,6 +2,9 @@ import random
 from edsl.surveys import Survey
 from edsl.questions import QuestionMultipleChoice
 from edsl.scenarios.ScenarioList import ScenarioList
+from edsl.language_models.LanguageModelOpenAIThreeFiveTurbo import (
+    LanguageModelOpenAIThreeFiveTurbo,
+)
 
 random.seed("agents are cool")
 
@@ -9,6 +12,8 @@ flip_results = [
     {"coin_flip_observed": random.choice(["heads", "tails"])} for _ in range(10)
 ]
 flip_scenarios = ScenarioList.gen(flip_results)
+
+m = LanguageModelOpenAIThreeFiveTurbo(use_cache=False)
 
 q1 = QuestionMultipleChoice(
     question_text="""You just observed a coin flip that had a value {{coin_flip_observed}}. 
@@ -25,5 +30,5 @@ q2 = QuestionMultipleChoice(
 s = Survey(questions=[q1, q2])
 s.add_targeted_memory(focal_question="q2", prior_question="q1")
 
-results = s.by(flip_scenarios).run()
+results = s.by(flip_scenarios).by(m).run()
 results.select("q1", "q2", "scenario.*").print()
