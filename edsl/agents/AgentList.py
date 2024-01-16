566a768888ca0300a5ce617010c72aadf16ebdee
John Horton <john.joseph.horton@gmail.com>
2024-01-16 10:45:25 -0500

    Adding test for AgentList

--- edsl/agents/AgentList.py
+++ edsl/agents/AgentList.py
@@ -16,18 +16,9 @@ class AgentList(UserList, Base, AgentListExportMixin):
     def to(self, question_or_survey: Union["Question", "Survey"]):
         return question_or_survey.by(*self)
 
-    def update_traits(self, new_attributes: list):
-        for agent in self.data:
-            agent.update_traits(new_attributes)
-
-    def print(self, html=False):
-        html = ""
-        for agent in self.data:
-            html += agent.dict_to_html()
-        from edsl.utilities.interface import gen_html_sandwich
-        from edsl.utilities.interface import view_html
-
-        view_html(gen_html_sandwich(html))
+    # def update_traits(self, new_attributes: list):
+    #     for agent in self.data:
+    #         agent.update_traits(new_attributes)
 
     def to_dict(self):
         return {"agent_list": [agent.to_dict() for agent in self.data]}
@@ -52,24 +43,4 @@ class AgentList(UserList, Base, AgentListExportMixin):
 
 
 if __name__ == "__main__":
-    a = AgentList([1, 2, 3])
-    print(a)
-
-    from edsl.questions.QuestionMultipleChoice import QuestionMultipleChoice
-    from edsl.agents.Agent import Agent
-
-    q = QuestionMultipleChoice(
-        question_text="How are you feeling?",
-        question_options=["Very sad.", "Sad.", "Neutral.", "Happy.", "Very happy."],
-        question_name="feelings",
-    )
-
-    a = Agent(traits={"feeling": "Very sad."})
-    agent_list = AgentList([a])
-    results = agent_list.to(q).run()
-
-    print(results)
-
-    results2 = q.by(agent_list).run()
-
-    print(results2)
+    pass
