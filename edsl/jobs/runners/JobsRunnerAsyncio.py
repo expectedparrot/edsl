5bdf88a3546357918ebf8e27eea120d3b648955f
John Horton <john.joseph.horton@gmail.com>
2024-01-17 08:39:11 -0500

    Clarity in async job runner

--- edsl/jobs/runners/JobsRunnerAsyncio.py
+++ edsl/jobs/runners/JobsRunnerAsyncio.py
@@ -1,10 +1,10 @@
 import asyncio
 
+from typing import Coroutine, List
+
 from edsl.jobs import Jobs
 from edsl.results import Results, Result
 from edsl.jobs.JobsRunner import JobsRunner
-from typing import Coroutine
-
 
 from edsl.utilities.decorators import jupyter_nb_handler
 
@@ -12,14 +12,46 @@ from edsl.utilities.decorators import jupyter_nb_handler
 class JobsRunnerAsyncio(JobsRunner):
     runner_name = "asyncio"
 
-    def __init__(self, jobs: Jobs):
-        super().__init__(jobs)
+    async def run_async(
+        self, n=1, verbose=False, sleep=0, debug=False, progress_bar=False
+    ) -> Results:
+        """Creates the tasks, runs them asynchronously, and returns the results as a Results object."""
 
-    def get_prompts(self, results, answer):
+        tasks = self._create_all_interview_tasks(self.interviews)
+        data = await asyncio.gather(*tasks)
+        return Results(survey=self.jobs.survey, data=data)
+
+    def _create_all_interview_tasks(self, interviews) -> List[asyncio.Task]:
+        """Creates an awaitable task for each interview"""
+        tasks = []
+        for i, interview in enumerate(interviews):
+            interviewing_task = self._interview_task(interview, i)
+            tasks.append(asyncio.create_task(interviewing_task))
+        return tasks
+
+    async def _interview_task(self, interview, i):
+        # Assuming async_conduct_interview and Result are defined and work asynchronously
+        answer, prompt_data = await interview.async_conduct_interview(debug=False)
+        user_prompts, system_prompts = self._get_prompts(answer, prompt_data)
+
+        result = Result(
+            agent=interview.agent,
+            scenario=interview.scenario,
+            model=interview.model,
+            iteration=i,
+            answer=answer,
+            prompt=user_prompts | system_prompts,
+        )
+        return result
+
+    def _get_prompts(self, answer, prompt_data):
+        """Gets the prompts used in the survey."""
         answer_names = [k for k in answer.keys() if not k.endswith("_comment")]
         try:
-            user_prompts = [result["prompts"]["user_prompt"] for result in results]
-            system_prompts = [result["prompts"]["system_prompt"] for result in results]
+            user_prompts = [result["prompts"]["user_prompt"] for result in prompt_data]
+            system_prompts = [
+                result["prompts"]["system_prompt"] for result in prompt_data
+            ]
         except KeyError:
             user_prompts = [None] * len(answer_names)
             system_prompts = [None] * len(answer_names)
@@ -31,34 +63,6 @@ class JobsRunnerAsyncio(JobsRunner):
         system_prompts = append_type(system_prompts, "system_prompt")
 
         return user_prompts, system_prompts
-
-    async def run_async(
-        self, n=1, verbose=False, sleep=0, debug=False, progress_bar=False
-    ) -> Results:
-        """Asynchronous method to run a collection of interviews."""
-
-        async def process_task(interview, i):
-            # Assuming async_conduct_interview and Result are defined and work asynchronously
-            answer, results = await interview.async_conduct_interview(debug=debug)
-            user_prompts, system_prompts = self.get_prompts(results, answer)
-            result = Result(
-                agent=interview.agent,
-                scenario=interview.scenario,
-                model=interview.model,
-                iteration=i,
-                answer=answer,
-                prompt=user_prompts | system_prompts,
-            )
-            return result
-
-        async def main(interviews):
-            tasks = [
-                process_task(interview, i) for i, interview in enumerate(interviews)
-            ]
-            results = await asyncio.gather(*tasks)
-            return results
-
-        return Results(survey=self.jobs.survey, data=await main(self.interviews))
 
     @jupyter_nb_handler
     def run(
