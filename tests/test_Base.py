802af9c74e3bf0e8412fd2eb3672a2c39d82d048
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-01-21 17:26:02 -0500

    fixing survey repr eval

--- tests/test_Base.py
+++ tests/test_Base.py
@@ -2,9 +2,6 @@ import pytest
 from edsl.Base import RegisterSubclassesMeta
 from edsl.questions import QuestionMultipleChoice
 
-## TODO: no repr test for Survey - it doesn't work is excluded for Survey because it is not working
-exclude_repr_test = ["Survey"]
-
 
 class TestBaseModels:
     pass
@@ -13,19 +10,14 @@ class TestBaseModels:
 def create_test_function(child_class):
     @staticmethod
     def base_test_func():
-        print(f"Now testing: {child_class}")
-        try:
-            e = child_class()
-            assert hasattr(e, "example")
-            assert hasattr(e, "to_dict")
-            d = {
-                child_class.__name__: child_class,
-                "QuestionMultipleChoice": QuestionMultipleChoice,
-            }
-            if child_class.__name__ not in exclude_repr_test:
-                assert eval(repr(e), d) == e
-        except Exception as e:
-            pytest.fail(f"Error running {child_class}: {e}")
+        e = child_class()
+        assert hasattr(e, "example")
+        assert hasattr(e, "to_dict")
+        d = {
+            child_class.__name__: child_class,
+            "QuestionMultipleChoice": QuestionMultipleChoice,
+        }
+        assert eval(repr(e), d) == e
 
     return base_test_func
 
