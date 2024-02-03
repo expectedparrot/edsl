75a38e821f117a6bfaef5dfa67d33e60416cd530
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-02-02 19:23:48 -0500

    toned down stress test

--- tests/jobs/test_JobsStress.py
+++ tests/jobs/test_JobsStress.py
@@ -1,7 +1,7 @@
 from edsl.agents import Agent
 from edsl.data import CRUD
 from edsl.jobs import Jobs
-from edsl.questions import QuestionMultipleChoice
+from edsl.questions import QuestionFreeText
 from edsl.scenarios import Scenario
 from edsl.surveys import Survey
 
@@ -14,18 +14,17 @@ def test_jobs_stress(test_language_model_good_fixture):
         model="fake model",
         parameters={"temperature": 0.5},
     )
-    q = QuestionMultipleChoice(
+    q = QuestionFreeText(
         question_text="How are you?",
-        question_options=["Good", "Great", "OK", "Bad"],
         question_name="how_feeling",
     )
     job = Jobs(
         survey=Survey(name="Test Survey", questions=[q]),
-        agents=[Agent(traits={"trait1": f"value{x}"}) for x in range(100)],
+        agents=[Agent(traits={"trait1": f"value{x}"}) for x in range(10)],
         models=[m],
-        scenarios=[Scenario({"price": x, "quantity": 2}) for x in range(100)],
+        scenarios=[Scenario({"price": x, "quantity": 2}) for x in range(10)],
     )
     results = job.run()
     cached_results = CRUD.get_all_LLMOutputData()
-    assert len(CRUD.get_all_LLMOutputData()) == 10000
-    assert len(results) == 10000
+    assert len(cached_results) == 100
+    assert len(results) == 100
