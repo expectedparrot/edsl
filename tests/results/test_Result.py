645351f38e993bd44eb8bcf568c4f08658997036
john <=>
2024-02-11 09:13:20 -0500

    Making agent name available in results object

--- tests/results/test_Result.py
+++ tests/results/test_Result.py
@@ -3,3 +3,49 @@ from edsl.results.Result import main
 
 def test_result_main():
     main()
+
+
+from edsl.results.Result import Result
+
+from edsl import Agent
+from edsl import Scenario
+from edsl.language_models import LanguageModel
+
+def test_constructor():
+    result = Result(agent = Agent.example(), 
+               scenario = Scenario.example(), 
+               model = LanguageModel.example(), 
+               iteration = 1, 
+               answer = "answer", 
+               prompt = {"key": "value"})
+    
+    agent_traits = Agent.example().traits
+    agent_traits["agent_name"] = "Agent_0"
+    
+    try:
+        assert result.sub_dicts['agent'] == agent_traits
+    except:
+        print(result.sub_dicts['agent'])
+        print(agent_traits)
+        breakpoint()
+
+
+
+def test_constructor():
+    result = Result(agent = Agent(name = "Arsenio Billingham"), 
+               scenario = Scenario.example(), 
+               model = LanguageModel.example(), 
+               iteration = 1, 
+               answer = "answer", 
+               prompt = {"key": "value"})
+    
+    agent_traits = Agent.example().traits
+    agent_traits["agent_name"] = "Arsenio Billingham"
+    
+    try:
+        assert result.sub_dicts['agent'] == agent_traits
+    except:
+        print(result.sub_dicts['agent'])
+        print(agent_traits)
+        breakpoint()
+
