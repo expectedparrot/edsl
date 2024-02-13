a51fa0b90f993997ba1fed56d0b4502061b15b3b
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-02-13 06:15:00 -0500

    coop client supports surveys

--- edsl/coop/coop.py
+++ edsl/coop/coop.py
@@ -3,6 +3,7 @@ import requests
 from typing import Any, Optional, Type, Union
 from edsl import CONFIG
 from edsl.questions import Question
+from edsl.surveys import Survey
 
 
 api_url = {
@@ -66,11 +67,9 @@ class Coop:
         self._resolve_server_response(response)
         return response.json()
 
-    def get_question(self, question_id: int) -> Type[Question]:
+    def get_question(self, id: int) -> Type[Question]:
         """Retrieves a Question object by id."""
-        response = self._send_server_request(
-            uri=f"api/v0/questions/{question_id}", method="GET"
-        )
+        response = self._send_server_request(uri=f"api/v0/questions/{id}", method="GET")
         self._resolve_server_response(response)
         return Question.from_dict(json.loads(response.json().get("json_string")))
 
@@ -88,10 +87,53 @@ class Coop:
         ]
         return questions
 
-    def delete_question(self, question_id: int) -> dict:
+    def delete_question(self, id: int) -> dict:
         """Deletes a question from the coop."""
         response = self._send_server_request(
-            uri=f"api/v0/questions/{question_id}", method="DELETE"
+            uri=f"api/v0/questions/{id}", method="DELETE"
+        )
+        self._resolve_server_response(response)
+        return response.json()
+
+    # Surveys METHODS
+    def create_survey(self, survey: Type[Survey], public: bool = False) -> dict:
+        """
+        Creates a Question object.
+        - `survey`: the EDSL Survey to be sent.
+        - `public`: whether the survey should be public (defaults to False)
+        """
+        response = self._send_server_request(
+            uri="api/v0/surveys",
+            method="POST",
+            payload={"json_string": json.dumps(survey.to_dict()), "public": public},
+        )
+        self._resolve_server_response(response)
+        return response.json()
+
+    def get_survey(self, id: int) -> Type[Survey]:
+        """Retrieves a Survey object by id."""
+        response = self._send_server_request(uri=f"api/v0/surveys/{id}", method="GET")
+        self._resolve_server_response(response)
+        return Survey.from_dict(json.loads(response.json().get("json_string")))
+
+    @property
+    def surveys(self) -> list[dict[str, Union[int, Survey]]]:
+        """Retrieves all Surveys."""
+        response = self._send_server_request(uri="api/v0/surveys", method="GET")
+        self._resolve_server_response(response)
+        surveys = [
+            {
+                "id": q.get("id"),
+                "survey": Survey.from_dict(json.loads(q.get("json_string"))),
+            }
+            for q in response.json()
+        ]
+        return surveys
+
+    def delete_survey(self, id: int) -> dict:
+        """Deletes a Survey from the coop."""
+        response = self._send_server_request(
+            uri=f"api/v0/surveys/{id}", method="DELETE"
         )
         self._resolve_server_response(response)
         return response.json()
@@ -99,11 +141,8 @@ class Coop:
 
 if __name__ == "__main__":
     from edsl.coop import Coop
-    from edsl.questions import QuestionMultipleChoice
-    from edsl.questions import QuestionCheckBox
-    from edsl.questions import QuestionFreeText
 
-    API_KEY = "X27Nqvl4oPxd_5Dt6oJbF9r2Myh_44Pit_851Ap7V7w"
+    API_KEY = "p-llmNVgNM8pnzCWZQ6-sDCdlMgRgithISctb_9yzqU"
     RUN_MODE = "development"
     coop = Coop(api_key=API_KEY, run_mode=RUN_MODE)
 
@@ -112,13 +151,20 @@ if __name__ == "__main__":
     coop.headers
     coop.url
 
-    # check jobs on server (should be an empty list)
+    ##############
+    # A. QUESTIONS
+    ##############
+    from edsl.questions import QuestionMultipleChoice
+    from edsl.questions import QuestionCheckBox
+    from edsl.questions import QuestionFreeText
+
+    # check questions on server (should be an empty list)
     coop.questions
     for question in coop.questions:
         coop.delete_question(question.get("id"))
 
     # get a question that does not exist (should return None)
-    coop.get_question(question_id=1)
+    coop.get_question(id=1)
 
     # now post a Question
     coop.create_question(QuestionMultipleChoice.example())
@@ -129,10 +175,40 @@ if __name__ == "__main__":
     coop.questions
 
     # or get question by id
-    coop.get_question(question_id=1)
+    coop.get_question(id=1)
 
     # delete the question
-    coop.delete_question(question_id=2)
+    coop.delete_question(id=1)
 
-    # check all questions (should be an empty list)
+    # check all questions
     coop.questions
+
+    ##############
+    # B. Surveys
+    ##############
+    from edsl.surveys import Survey
+
+    # check surveys on server (should be an empty list)
+    coop.surveys
+    for survey in coop.surveys:
+        coop.delete_survey(survey.get("id"))
+
+    # get a survey that does not exist (should return None)
+    coop.get_survey(id=1)
+
+    # now post a Survey
+    coop.create_survey(Survey.example())
+    coop.create_survey(Survey.example(), public=False)
+    coop.create_survey(Survey.example(), public=True)
+
+    # check all surveys
+    coop.surveys
+
+    # or get survey by id
+    coop.get_survey(id=1)
+
+    # delete the survey
+    coop.delete_survey(id=1)
+
+    # check all surveys
+    coop.surveys
