6e107501445c856dccfce712c23a4bb1a9fa4087
john <=>
2024-02-14 05:11:03 -0500

    Better printing of answers and prompts for results objects

--- edsl/results/Result.py
+++ edsl/results/Result.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 from collections import UserDict
 from typing import Any, Type
 
-import io
-from rich.console import Console
 from rich.table import Table
 
 from IPython.display import display
@@ -15,6 +13,22 @@ from edsl.scenarios import Scenario
 from edsl.utilities import is_notebook
 
 from edsl.Base import Base
+
+from collections import UserDict 
+
+class PromptDict(UserDict):
+
+    def rich_print(self):
+        """Displays an object as a table."""
+        table = Table(title = "")
+        table.add_column("Attribute", style="bold")
+        table.add_column("Value")
+
+        to_display = self
+        for attr_name, attr_value in to_display.items():
+            table.add_row(attr_name, repr(attr_value))
+
+        return table
 
 
 def agent_namer_closure():
@@ -158,6 +172,8 @@ class Result(Base, UserDict):
 
     def rich_print(self):
         """Displays an object as a table."""
+        #from edsl.utilities import print_dict_with_rich
+        from rich import print
         table = Table(title="Result")
         table.add_column("Attribute", style="bold")
         table.add_column("Value")
@@ -165,7 +181,13 @@ class Result(Base, UserDict):
         to_display = self.__dict__.copy()
         data = to_display.pop("data", None)
         for attr_name, attr_value in to_display.items():
-            table.add_row(attr_name, repr(attr_value))
+            if hasattr(attr_value, "rich_print"):
+                table.add_row(attr_name, attr_value.rich_print())
+            elif isinstance(attr_value, dict):
+                a = PromptDict(attr_value)
+                table.add_row(attr_name, a.rich_print())
+            else:
+                table.add_row(attr_name, repr(attr_value))
         return table
 
     def __repr__(self):
