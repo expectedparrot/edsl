70ef7fd1185d3728f98d31b2895895d00d0fd47f
john <=>
2024-02-20 08:55:46 -0500

    Improving status tracking with enum of status

--- edsl/jobs/Interview.py
+++ edsl/jobs/Interview.py
@@ -4,6 +4,8 @@ import logging
 import textwrap
 from collections import UserList
 from typing import Any, Type, List, Generator, Callable, List
+from collections import defaultdict 
+
 from edsl import CONFIG
 from edsl.agents import Agent
 from edsl.exceptions import InterviewErrorPriorTaskCanceled, InterviewTimeoutError
@@ -20,25 +22,8 @@ from edsl.surveys.base import EndOfSurvey
 from edsl.jobs.buckets import ModelBuckets
 from edsl.jobs.token_tracking import TokenUsage, InterviewTokenUsage
 
-## Ideas: 
-## https://github.com/openai/openai-cookbook/blob/main/examples/api_request_parallel_processor.py
+from edsl.jobs.task_management import InterviewStatusDictionary, QuestionTaskCreator, TasksList
 
-class TasksList(UserList):
-
-    def status(self, debug=False):
-        if debug:
-            for task in self:
-                print(f"Task {task.edsl_name}")
-                print(f"\t DEPENDS ON: {task.depends_on}")
-                print(f"\t DONE: {task.done()}")
-                print(f"\t CANCELLED: {task.cancelled()}")
-                if not task.cancelled():
-                    if task.done():
-                        print(f"\t RESULT: {task.result()}")
-                    else:
-                        print(f"\t RESULT: None - Not done yet")  
-
-            print("---------------------")
 
 # create logger
 logger = logging.getLogger(__name__)
@@ -59,125 +44,6 @@ logger.addHandler(fh)
 logger.info("Interview.py loaded")
 
 TIMEOUT = float(CONFIG.get("API_CALL_TIMEOUT_SEC"))
-
-class QuestionTaskCreator(UserList):
-    """Class to create and manage question tasks with dependencies.
-    It is a UserList with all the tasks that must be completed before the focal task can be run.
-    When called, it returns an asyncio.Task that depends on the tasks that must be completed before it can be run.
-    """
-
-    def __init__(self, *, question: Question, func: Callable, model_buckets: ModelBuckets, token_estimator: Callable = None):
-        super().__init__([])
-        self.func = func
-        self.question = question
-        self.model_buckets = model_buckets
-        self.waiting = False
-        self.from_cache = False
-        self.token_estimator = token_estimator
-
-        self.cached_token_usage = TokenUsage(from_cache = True)
-        self.new_token_usage = TokenUsage(from_cache = False)
-
-    def add_dependency(self, task):
-        """Adds a dependency to the list of dependencies."""
-        self.append(task)
-
-    def generate_task(self, debug) -> asyncio.Task:
-        """Creates a task that depends on the passed-in dependencies."""
-        task = asyncio.create_task(self._run_task_async(debug))
-        task.edsl_name = self.question.question_name
-        task.depends_on = [x.edsl_name for x in self] 
-        return task
-    
-    def estimated_tokens(self) -> int:
-        """Estimates the number of tokens that will be required to run the focal task."""
-        # TODO: Um, actually compute this.
-        token_estimate = self.token_estimator(self.question)
-        #breakpoint()
-        return token_estimate
-    
-    def token_usage(self) -> dict:
-        """Returns the token usage for the task."""
-        return {'cached_tokens': self.cached_token_usage, 'new_tokens': self.new_token_usage}
-
-    async def _run_focal_task(self, debug) -> 'Answers':
-        """Runs the focal task i.e., the question that we are interested in answering.
-        It is only called after all the dependency tasks are completed. 
-        """
-        self.requests_bucket = self.model_buckets.requests_bucket
-        self.tokens_bucket = self.model_buckets.tokens_bucket
-
-        # TODO: This isn't quite right because while you are waiting for the 
-        # requests token, the tokens bucket might get exhaused - though actually that's fine because
-        # you already reserved them. 
-        logger.info(f"Current bucket tokens balance: {self.tokens_bucket.tokens}")
-        requested_tokens = self.estimated_tokens()
-        logger.info(f"Requesting {requested_tokens} tokens for {self.question.question_name}") 
-        if (estimated_wait_time := self.tokens_bucket.wait_time(requested_tokens)) > 0:
-            #print("Pausing for TPM")
-            logger.info(f"Estimated time until tokens are available: {estimated_wait_time}")
-            self.waiting = True
-        await self.tokens_bucket.get_tokens(requested_tokens)
-        self.waiting = False
-        logger.info("Token funds acquired!")
-
-
-        ## Requests per minute check
-        logger.info(f"Request bucket balance: {self.requests_bucket.tokens}")
-        if (estimated_wait_time := self.requests_bucket.wait_time(1)) > 0:
-            logger.info(f"Pausing {self.question.question_name} for {estimated_wait_time} seconds to stay under RPM limit")
-            #print("Pausing for RPM")
-            self.waiting = True
-        await self.requests_bucket.get_tokens(1)
-        self.waiting = False
-        logger.info(f"Requests funds acquired!")
-
-        results = await self.func(self.question, debug)
-
-        # If the result was cached, we don't need to use any tokens
-        if 'cached_response' in results:
-            if results['cached_response']:
-                logger.info(f"Result for {self.question.question_name} was cached.")
-                # put the tokens back; didn't need them
-                self.tokens_bucket.add_tokens(requested_tokens)
-                self.requests_bucket.add_tokens(1)
-                self.from_cache = 1
-
-        # Track token usage
-                
-        tracker = self.cached_token_usage if self.from_cache else self.new_token_usage
-        usage = results.get('usage', {'prompt_tokens': 0, 'completion_tokens': 0})
-        prompt_tokens = usage.get("prompt_tokens", 0)
-        completion_tokens = usage.get("completion_tokens", 0)
-        tracker.add_tokens(prompt_tokens = prompt_tokens, completion_tokens = completion_tokens)
-
-        return results
-
-    async def _run_task_async(self, debug) -> 'Answers':
-        """Runs the task asynchronously, awaiting the tasks that must be completed before this one can be run."""
-        logger.info(f"Running task for {self.question.question_name}")
-        try:
-            # This is waiting for the tasks that must be completed before this one can be run.
-            # This does *not* use the return_exceptions = True flag, so if any of the tasks fail,
-            # it throws the exception immediately, which is what we want.
-            await asyncio.gather(*self)
-        except asyncio.CancelledError:
-            logger.info(f"Task for {self.question.question_name} was cancelled, most likely because it was skipped.")
-            raise
-        except Exception as e:
-            logger.error(f"Required tasks for {self.question.question_name} failed: {e}")
-            # turns the parent exception into a custom exception
-            # So the task gets canceled but this InterviewErrorPriorTaskCanceled exception
-            # So we never get the question details we need.
-            raise InterviewErrorPriorTaskCanceled(
-                f"Required tasks failed for {self.question.question_name}"
-            ) from e
-        else:
-            logger.info(f"Tasks for {self.question.question_name} completed")
-            # This is the actual task that we want to run.
-            #results = await self.func(question, debug)
-            #return results
-            return await self._run_focal_task(debug)
 
 
 class Interview:
@@ -202,20 +68,14 @@ class Interview:
         self.verbose = verbose
         self.answers: dict[str, str] = Answers()  # will get filled in
 
-        self.dag = self.survey.dag(
-            textify=True
-        )  # the DAG tells us what questions depend on what other questions
+        self.dag = self.survey.dag(textify=True)  
         self.to_index = {
             name: index for index, name in enumerate(self.survey.question_names)
         }
-        # Make huge-ass bucket for testing purposes so it goes quickly
-        # TODO: Check config if in testing mode and use giant bucket
-        #self.bucket = bucket or TokenBucket(capacity=2000000000000,refill_rate=1000000000000)
 
         logger.info(f"Interview instantiated")
         self.task_creators = {}
 
-    
 
     @property
     def token_usage(self) -> dict:
@@ -227,14 +87,16 @@ class Interview:
             new_tokens += token_usage["new_tokens"]
         return InterviewTokenUsage(new_token_usage=new_tokens, cached_token_usage=cached_tokens)
 
-    @property
-    def num_tasks_waiting(self):
-        return sum([task_creator.waiting for task_creator in self.task_creators.values()])
-    
-    @property
-    def num_from_cache(self):
-        return sum([task_creator.from_cache for task_creator in self.task_creators.values()])
 
+    @property
+    def interview_status(self) -> InterviewStatusDictionary:
+        """Returns a dictionary mapping task status codes to counts"""
+        status_dict = InterviewStatusDictionary()
+        for task_creator in self.task_creators.values():
+            status_dict[task_creator.task_status] += 1
+            status_dict['number_from_cache'] += task_creator.from_cache
+        return status_dict
+    
     async def async_conduct_interview(
         self, 
         model_buckets: ModelBuckets, 
@@ -257,12 +119,7 @@ class Interview:
         """
 
         self.tasks, self.invigilators = self._build_question_tasks(debug = debug, model_buckets = model_buckets)
-        
-        self.tasks.status()
-
-        # when return_exceptions=False, it will just raise the exception
-        # and break the loop; otherwise it returns.                
-        
+                
         #debug = True
         return_exceptions = not debug 
         
@@ -273,8 +130,6 @@ class Interview:
 
         valid_results = list(self._extract_valid_results(self.tasks, self.invigilators))
 
-        logger.info(f"Total of tasks requested:\t {len(self.tasks)}")
-        logger.info(f"Number of valid results:\t {len(valid_results)}")
         return self.answers, valid_results
 
     #conduct_interview = sync_wrapper(async_conduct_interview)
@@ -290,8 +145,6 @@ class Interview:
         # there should be one one invigilator for each task
         assert len(self.tasks) == len(self.invigilators)
         warning_printed = False
-
-        self.tasks.status()
 
         for task, invigilator in zip(self.tasks, self.invigilators):
             logger.info(f"Iterating through task: {task}")
@@ -361,9 +214,9 @@ class Interview:
         """Creates a task that depends on the passed-in dependencies that are awaited before the task is run.
         """
         task_creator = QuestionTaskCreator(question = question, 
-                                           func=self._answer_question_and_record_task, 
+                                            func=self._answer_question_and_record_task, 
                                             token_estimator=self._get_estimated_request_tokens,
-                                           model_buckets = model_buckets
+                                            model_buckets = model_buckets
                                            )
         [task_creator.add_dependency(x) for x in tasks_that_must_be_completed_before]
         self.task_creators[question.question_name] = task_creator
@@ -397,7 +250,7 @@ class Interview:
     def _get_estimated_request_tokens(self, question):
         """Estimates the number of tokens that will be required to run the focal task."""
         invigilator = self.get_invigilator(question, debug=False)
-        #breakpoint()
+        # TODO: There should be a way to get a more accurate estimate.
         combined_text = ""
         for prompt in invigilator.get_prompts().values():
             if hasattr(prompt, "text"):
@@ -419,12 +272,12 @@ class Interview:
         """
         invigilator = self.get_invigilator(question, debug=debug)
         response: AgentResponseDict = await invigilator.async_answer_question()
-        #breakpoint()
+        # TODO: Move this back into actual agent response dict and enforce it.
         response["question_name"] = question.question_name
 
         self.answers.add_answer(response, question)
 
-        _ = self._cancel_skipped_questions(question)
+        self._cancel_skipped_questions(question)
 
         return response
     
@@ -432,7 +285,6 @@ class Interview:
     def _cancel_skipped_questions(self, current_question):
         """Cancels the tasks for questions that are skipped."""
         logger.info(f"Current question is {current_question.question_name}")
-        self.tasks.status()
         current_question_index = self.to_index[current_question.question_name]
         next_question = self.survey.rule_collection.next_question(q_now=current_question_index, answers=self.answers)
         next_question_index = next_question.next_q
