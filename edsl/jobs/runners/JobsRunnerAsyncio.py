f3e8af7e0b0bc6b842373365348d272293a4fb48
John Horton <john.joseph.horton@gmail.com>
2024-02-26 08:26:33 -0500

    Testing usage / token tracking

--- edsl/jobs/runners/JobsRunnerAsyncio.py
+++ edsl/jobs/runners/JobsRunnerAsyncio.py
@@ -1,12 +1,13 @@
 import time
 import asyncio
-from typing import Coroutine, List
+from typing import Coroutine, List, AsyncGenerator
 
 from rich.live import Live
 from rich.console import Console
 
 from edsl.results import Results, Result
 from edsl.jobs.JobsRunner import JobsRunner
+from edsl.jobs.Interview import Interview
 from edsl.utilities.decorators import jupyter_nb_handler
 
 from edsl.jobs.JobsRunnerStatusMixin import JobsRunnerStatusMixin
@@ -15,7 +16,7 @@ class JobsRunnerAsyncio(JobsRunner, JobsRunnerStatusMixin):
     runner_name = "asyncio"
 
     async def run_async(
-        self, n=1, verbose=False, sleep=0, debug=False, progress_bar=False):
+        self, n=1, verbose=False, sleep=0, debug=False, progress_bar=False) -> AsyncGenerator[Result, None]:
         """Creates the tasks, runs them asynchronously, and returns the results as a Results object."""
 
         tasks = self._create_all_interview_tasks(self.interviews, debug)
@@ -31,7 +32,7 @@ class JobsRunnerAsyncio(JobsRunner, JobsRunnerStatusMixin):
             tasks.append(asyncio.create_task(interviewing_task))
         return tasks
 
-    async def _interview_task(self, interview, i, debug):
+    async def _interview_task(self, interview: Interview, i: int, debug: bool) -> Result:
         # Assuming async_conduct_interview and Result are defined and work asynchronously
         model_buckets = self.bucket_collection[interview.model]
         answer, valid_results = await interview.async_conduct_interview(debug=debug, model_buckets = model_buckets)
