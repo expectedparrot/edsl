b43bd32bba0c3ea3969d4c266ba0b9a7a1cf897a
john <=>
2024-03-09 10:36:29 -0500

    Adding testing

--- tests/jobs/task_management/test_InterviewStatusDictionary.py
+++ tests/jobs/task_management/test_InterviewStatusDictionary.py
@@ -1,6 +1,7 @@
-from edsl.jobs.task_management import InterviewStatusDictionary
+import pytest 
 
-from edsl.jobs.task_management import TaskStatus
+from edsl.jobs.task_management import InterviewStatusDictionary
+from edsl.jobs.task_management import TaskStatus, TaskStatusDescriptor
 
 def test_instantiation():
     i = InterviewStatusDictionary()
@@ -13,3 +14,31 @@ def test_instantiation():
     i2 = InterviewStatusDictionary()
     i3 = i1 + i2
     assert isinstance(i3, InterviewStatusDictionary)
+
+
+def test_check_enum():
+    data = {task_status:0 for task_status in TaskStatus}
+    data["number_from_cache"] = 0
+    i = InterviewStatusDictionary(data = data)
+
+    # expect an assertion error
+    with pytest.raises(AssertionError):
+        data = {task_status:0 for task_status in TaskStatus}
+        import random
+        to_delete = random.choice(list(TaskStatus))
+        data.pop(to_delete)
+        i = InterviewStatusDictionary(data = data)
+
+
+def test_check_descriptor():
+
+    class FakeClass:
+        task = TaskStatusDescriptor()
+
+    a = FakeClass()
+    a.task = TaskStatus.API_CALL_IN_PROGRESS
+
+    # can't assign a status that's not from the TaskStatus enum
+    with pytest.raises(ValueError):
+        b = FakeClass()
+        b.task = "poo"
