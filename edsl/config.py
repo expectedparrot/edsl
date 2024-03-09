112dc029f9e7f6f5b4e26b2125eb1dac507fa5d7
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-03-09 10:09:59 -0500

    package specific env vars start with EDSL_

--- edsl/config.py
+++ edsl/config.py
@@ -34,6 +34,21 @@ CONFIG_MAP = {
         "allowed": None,
         "user_message": "What is the maximum number of seconds to wait for an API call to return?",
     },
+    "EDSL_BACKOFF_START_SEC": {
+        "default": "1",
+        "allowed": None,
+        "user_message": "What is the number of seconds to wait before retrying a failed API call?",
+    },
+    "EDSL_MAX_BACKOFF_SEC": {
+        "default": "60",
+        "allowed": None,
+        "user_message": "What is the maximum number of seconds to wait before retrying a failed API call?",
+    },
+    "EDSL_MAX_ATTEMPTS": {
+        "default": "5",
+        "allowed": None,
+        "user_message": "What is the maximum number of times to retry a failed API call?",
+    },
     "EXPECTED_PARROT_API_KEY": {
         "default": "local",
         "allowed": None,
@@ -53,21 +68,6 @@ CONFIG_MAP = {
         "default": None,
         "allowed": None,
         "user_message": "Please provide your Google API key (https://console.cloud.google.com/apis/credentials).",
-    },
-    "BACKOFF_START_SEC": {
-        "default": "1",
-        "allowed": None,
-        "user_message": "What is the number of seconds to wait before retrying a failed API call?",
-    },
-    "MAX_BACKOFF_SEC": {
-        "default": "60",
-        "allowed": None,
-        "user_message": "What is the maximum number of seconds to wait before retrying a failed API call?",
-    },
-    "MAX_ATTEMPTS": {
-        "default": "5",
-        "allowed": None,
-        "user_message": "What is the maximum number of times to retry a failed API call?",
     },
 }
 
