5a8ba6c6e4e3e6f972f541f41372ce1f3b96e45a
john <=>
2024-03-10 16:40:06 -0400

    Improving docstrings

--- edsl/agents/AgentList.py
+++ edsl/agents/AgentList.py
@@ -1,40 +1,48 @@
+"""A module for lists of agents."""
 from __future__ import annotations
-import io
 from collections import UserList
 from typing import Optional, Union
 
-from rich.console import Console
 from rich.table import Table
 
 from edsl.agents import Agent
 from edsl.agents.AgentListExportMixin import AgentListExportMixin
 from edsl.Base import Base
 
-
 class AgentList(UserList, Base, AgentListExportMixin):
+    """A list of Agents.
+    
+    This is a list of Agents.
+    """
+    
     def __init__(self, data: Optional[list] = None):
+        """Initialize a new AgentList."""
         if data is not None:
             super().__init__(data)
         else:
             super().__init__()
 
-    def to(self, question_or_survey: Union["Question", "Survey"]):
+    def to(self, question_or_survey: Union["Question", "Survey"]) -> 'Jobs':
+        """Return a Job with a question or survey taken by the agent."""
         return question_or_survey.by(*self)
 
     def to_dict(self):
+        """Return dictionary of AgentList to serialization."""
         return {"agent_list": [agent.to_dict() for agent in self.data]}
 
     @classmethod
     def from_dict(cls, data: dict) -> "AgentList":
-        """Deserializes the dictionary back to an Agent List object."""
+        """Deserialize the dictionary back to an AgentList object."""
         agents = [Agent.from_dict(agent_dict) for agent_dict in data["agent_list"]]
         return cls(agents)
 
     @classmethod
     def example(cls):
+        """Return an example AgentList."""
         return cls([Agent.example(), Agent.example()])
 
     def code(self):
+        """Return code to construct an AgentList."""
         lines = [
             "from edsl.agents.Agent import Agent",
             "from edsl.agents.AgentList import AgentList",
@@ -43,7 +51,7 @@ class AgentList(UserList, Base, AgentListExportMixin):
         return lines
 
     def rich_print(self):
-        """Displays an object as a table."""
+        """Display an object as a rich table."""
         table = Table(title="AgentList")
         table.add_column("Agents", style="bold")
         for agent in self.data:
