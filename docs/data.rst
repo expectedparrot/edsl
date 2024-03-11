99adc5997f246868990ff1272ee1ec66e9778416
robin <robin@goemeritus.com>
2024-03-11 12:41:31 -0400

    Edits for Docs

---
+++ docs/data.rst
@@ -0,0 +1,18 @@
+Data
+====
+
+Cache class
+--------------
+
+.. automodule:: edsl.data.Cache
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Database class
+-----------------------------
+
+.. automodule:: edsl.data.Database
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- edsl/report/ResultsOutputMixin.py
+++ edsl/report/ResultsOutputMixin.py
@@ -1,9 +1,12 @@
+"""This module contains the ResultsOutputMixin class, which is used to add output functions to the Results class."""
 from edsl.report.ReportOutputs import RegisterElementMeta as registery
 
 
 class ResultsOutputMixin:
+    """Mixin class for adding output functions to the Results class."""
+    
     def add_output_functions(self) -> None:
-        """Iterates through all output classes and adds a function to the Results class for each one."""
+        """Iterate through all output classes and add a function to the Results class for each one."""
         output_classes = registery.get_registered_classes().values()
         self.analysis_options = []
         for output_class in output_classes:
