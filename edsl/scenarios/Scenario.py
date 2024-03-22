db685acf084fb2adf2c9051942f6d9ad87d52a63
robin <robin@goemeritus.com>
2024-03-22 09:07:59 -0400

    Describe Scenario

--- edsl/scenarios/Scenario.py
+++ edsl/scenarios/Scenario.py
@@ -1,34 +1,32 @@
-"""A Scenario is a dictionary of parameters for creating different versions of questions.
+"""A Scenario is a dictionary with a key/value to parameterize a question.
 
 Constructing a Scenario
 -----------------------
 Key steps:
-* Create a question that takes a parameter in double braces, e.g.: `{{ item }}`
+* Create a question that takes a parameter in double braces, e.g.: 
 ``` 
 q = QuestionFreeText(
     question_name = "favorite_item",
     question_text = "What is your favorite {{ item }}?",
 )
 ```
-* Create a Scenario for the value that will replace the parameter:
+* Create a dictionary for the value that will replace the parameter and store it in a Scenario object:
 ```
 s = Scenario({"item": "color"})
 ```
-* Add the scenario to the question or survey when you run it using the `by` method:
+If multiple values will be used, create multiple Scenario objects in a list:
 ```
-q.by(scenario).run()
+s = [Scenario({"item": item}) for item in ["color", "food"]]
+```
+* Add the Scenario objects to the question when you run it with the `by` method:
+```
+results = q.by(s).run()
 ```
 If your question is part of a survey, add the Scenario objects to the survey:
 ```
 q1 = ...
 q2 = ...
-survey = Survey([q1, q2]).by([s]).run()
-```
-* Note: Multiple Scenario objects should be added to a survey as a list:
-```
-s1 = Scenario({"item": "color"})
-s2 = Scenario({"item": "food"})
-survey = Survey([q]).by([s1, s2]).run()
+results = Survey([q1, q2]).by(s).run()
 ```
 """
 
