68aaa7395aa27cdcc283698d472d27642e8b7ecb
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-04-02 09:34:50 -0400

    cleaning and adding TODOs

--- edsl/data/CacheEntry.py
+++ edsl/data/CacheEntry.py
@@ -1,6 +1,12 @@
 import datetime
-import time
 import hashlib
+
+# TODO: deal better with types
+# passing in
+#    parameters = "{'temperature': 0.5}"
+# vs
+#    parameters = '{"temperature": 0.5}'
+# yields different hashes
 
 
 class CacheEntry:
@@ -35,7 +41,7 @@ class CacheEntry:
         self, *, model, parameters, system_prompt, user_prompt, iteration
     ) -> str:
         """
-        Generate a key for the cache entry.
+        Generates a key for the cache entry.
 
         >>> CacheEntry.gen_key(model = "gpt-3.5-turbo", parameters = "{'temperature': 0.5}", system_prompt = "The quick brown fox jumps over the lazy dog.", user_prompt = "What does the fox say?", iteration = 1)
         '55ce2e13d38aa7fb6ec848053285edb4'
@@ -46,7 +52,7 @@ class CacheEntry:
     @property
     def key(self) -> str:
         """
-        Return the key for the cache entry.
+        Returns the key for the cache entry.
 
         >>> CacheEntry.example().key
         '55ce2e13d38aa7fb6ec848053285edb4'
@@ -54,48 +60,10 @@ class CacheEntry:
         d = {k: value for k, value in self.__dict__.items() if k in self.key_fields}
         return self.gen_key(**d)
 
-    def __eq__(self, other_entry: "CacheEntry") -> bool:
-        """Check if two cache entries are equal.
-
-        :param other_entry: The other cache entry to compare to.
-
-        >>> CacheEntry.example() == CacheEntry.example()
-        True
-        """
-        for field in self.all_fields:
-            if getattr(self, field) != getattr(other_entry, field):
-                return False
-        return True
-
-    @classmethod
-    def example_dict(cls) -> dict:
-        """Return an example dictionary of cache entries."""
-        entity = cls.example()
-        key = entity.key
-        return {key: entity}
-
-    @classmethod
-    def fetch_input_example(cls) -> dict:
-        """
-        Create an example input for a 'fetch' operation.
-        """
-        input = cls.example().to_dict()
-        _ = input.pop("timestamp")
-        _ = input.pop("output")
-        return input
-
-    @classmethod
-    def store_input_example(cls) -> dict:
-        """
-        Create an example input for a 'store' operation.
-        """
-        input = cls.example().to_dict()
-        _ = input.pop("timestamp")
-        input["response"] = input.pop("output")
-        return input
-
     def to_dict(self) -> dict:
-        """Return a dictionary representation of the cache entry."""
+        """
+        Returns a dictionary representation of a CacheEntry.
+        """
         return {
             "model": self.model,
             "parameters": self.parameters,
@@ -106,16 +74,34 @@ class CacheEntry:
             "timestamp": self.timestamp,
         }
 
+    @classmethod
+    def from_dict(cls, data: dict) -> "CacheEntry":
+        """
+        Initializes a CacheEntry object from its dictionary representation.
+        """
+        return cls(**data)
+
+    def __eq__(self, other: "CacheEntry") -> bool:
+        """
+        Checks if two CacheEntry objects are equal.
+        - Includes timestamp in the comparison.
+        """
+        for field in self.all_fields:
+            if getattr(self, field) != getattr(other, field):
+                return False
+        return True
+
     def __repr__(self) -> str:
+        """
+        Returns a string representation of a CacheEntry.
+        """
         return f"CacheEntry(model={self.model}, parameters={self.parameters}, system_prompt={self.system_prompt}, user_prompt={self.user_prompt}, output={self.output}, iteration={self.iteration}, timestamp={self.timestamp})"
 
     @classmethod
-    def from_dict(cls, data: dict) -> "CacheEntry":
-        """Create a cache entry from a dictionary representation."""
-        return cls(**data)
-
-    @classmethod
     def example(cls) -> "CacheEntry":
+        """
+        Returns a CacheEntry example.
+        """
         return CacheEntry(
             model="gpt-3.5-turbo",
             parameters={"temperature": 0.5},
@@ -126,24 +112,60 @@ class CacheEntry:
             timestamp=int(datetime.datetime.now(datetime.timezone.utc).timestamp()),
         )
 
+    @classmethod
+    def example_dict(cls) -> dict:
+        """
+        Returns an example dictionary with a single CacheEntry.
+        - This will be useful one level up, in the Cache class.
+        """
+        cache_entry = cls.example()
+        return {cache_entry.key: cache_entry}
+
+    @classmethod
+    def fetch_input_example(cls) -> dict:
+        """
+        Creates an example input for a 'fetch' operation.
+        - This will be useful one level up, in the Cache class.
+        """
+        input = cls.example().to_dict()
+        _ = input.pop("timestamp")
+        _ = input.pop("output")
+        return input
+
+    @classmethod
+    def store_input_example(cls) -> dict:
+        """
+        Creates an example input for a 'store' operation.
+        - This will be useful one level up, in the Cache class.
+        """
+        input = cls.example().to_dict()
+        _ = input.pop("timestamp")
+        input["response"] = input.pop("output")
+        return input
+
 
 def main():
     from edsl.data.CacheEntry import CacheEntry
 
     # an example of how a cache entry looks
-    CacheEntry.example()
+    cache_entry = CacheEntry.example()
     # the key gives the hash of the cache entry
-    CacheEntry.example().key
+    cache_entry.key
     # kind of a forward method, that will be used by Cache
-    CacheEntry.example().example_dict()
+    cache_entry.example_dict()
     # to dict / from dict
-    CacheEntry.example().to_dict()
-    CacheEntry.from_dict(CacheEntry.example().to_dict())
-
+    cache_entry.to_dict()
+    CacheEntry.from_dict(cache_entry.to_dict())
+    # TODO: this will be false because equality includes timestamp
+    CacheEntry.from_dict(cache_entry.to_dict()) == CacheEntry.example()
     # equality through checking one by one
     CacheEntry.example() == CacheEntry.example()
     # but could also check the hash
-    CacheEntry.example().key == CacheEntry.example().key
+    cache_entry.key == CacheEntry.example().key
+
+    # not sure what these are useful for yet
+    cache_entry.fetch_input_example()
+    cache_entry.store_input_example()
 
 
 if __name__ == "__main__":
