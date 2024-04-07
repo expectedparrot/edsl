53bbd3955cca8e926e6e0246c83ecc1cb0e0cc8e
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-04-07 13:03:33 -0400

    better config tests

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -1,62 +1,40 @@
 import os
-from getpass import getpass
 import pytest
-from unittest.mock import patch, mock_open
-from edsl.config import Config, CONFIG_MAP
-from edsl.exceptions import (
-    InvalidEnvironmentVariableError,
-    MissingEnvironmentVariableError,
-)
-
-# MOCK VARS
-MOCK_ENV_VARS = {
-    "EDSL_RUN_MODE": "development",
-    "OPENAI_API_KEY": "some_key",
-}
-# FIXTURES
-# capture the original get env before patching
-original_getenv = os.getenv
+from edsl.config import Config
+from edsl.exceptions import InvalidEnvironmentVariableError
 
 
-# patch get_env
-def mock_getenv(var_name, default=None):
-    """Mocks os.getenv()."""
-    return MOCK_ENV_VARS.get(var_name, original_getenv(var_name, default))
-
-
-@pytest.fixture(scope="function")
-def mock_env():
-    """Mock os.getenv() for testing."""
-    with patch("os.getenv", mock_getenv):
-        yield
-
-
-@pytest.fixture(scope="function")
-def test_config(mock_env):
-    """Yield a test config object to tests, with mock env var values."""
-    config = Config()
-    yield config
-
-
-# TESTS
-
-
-def test_config_store_and_load(test_config):
+def test_config_store_and_load(set_env_vars):
     """Config should store the & sets env vars."""
-    test_config.show()
-    # both in the object and in the env for optional vars that are given
-    assert test_config.EDSL_RUN_MODE == MOCK_ENV_VARS["EDSL_RUN_MODE"]
-    assert os.getenv("EDSL_RUN_MODE") == test_config.EDSL_RUN_MODE
+    set_env_vars(EDSL_RUN_MODE="development")
+    config = Config()
+    config.show()
+    assert config.EDSL_RUN_MODE == "development"
+    assert os.getenv("EDSL_RUN_MODE") == "development"
 
 
-def test_config_show_method(test_config, capsys):
-    test_config.show()
+def test_config_incorrect_run_mode(set_env_vars):
+    """Config should raise an error if the run mode is incorrect."""
+    set_env_vars(EDSL_RUN_MODE="incorrect")
+    with pytest.raises(InvalidEnvironmentVariableError):
+        config = Config()
+        print(config.EDSL_RUN_MODE)
+
+
+def test_config_back_to_normal():
+    config = Config()
+    assert config.EDSL_RUN_MODE == "development-testrun"
+
+
+def test_config_show_method(capsys):
+    config = Config()
+    config.show()
     captured = capsys.readouterr()
     assert "Here are the current configuration settings:" in captured.out
 
 
-def test_config_get_method(test_config, capsys):
-    assert test_config.get("EDSL_RUN_MODE") == test_config.EDSL_RUN_MODE
-    assert test_config.get("EDSL_DATABASE_PATH") == test_config.EDSL_DATABASE_PATH
+def test_config_get_method():
+    config = Config()
+    assert config.get("EDSL_DATABASE_PATH") is not None
     with pytest.raises(InvalidEnvironmentVariableError):
-        test_config.get("INVALID")
+        config.get("INVALID")
