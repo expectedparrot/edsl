7580cd176a7a80155f3e6b5b8fecc5a6651156f8
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-04-07 13:03:23 -0400

    a better way to change your test env vars

--- tests/language_models/test_env_setup.py
+++ tests/language_models/test_env_setup.py
@@ -1,45 +1,29 @@
-import pytest 
 import os
-from tempfile import TemporaryDirectory
-import dotenv
-
-@pytest.fixture
-def temp_env():
-    # Create a temporary directory
-    with TemporaryDirectory() as temp_dir:
-        # Create a custom .env file in the temporary directory
-        env_path = os.path.join(temp_dir, '.env')
-        with open(env_path, 'w') as f:
-            f.write("KEY=VALUE\nANOTHER_KEY=ANOTHER_VALUE")
-            f.write("\nOPENAI_API_KEY='a_fake_key'")
-
-        # Change the current working directory to the temporary directory
-        original_cwd = os.getcwd()
-        os.chdir(temp_dir)
-
-        os.environ.clear()
-
-        dotenv.load_dotenv(env_path, override=True)        
-        
-        yield
-        
-        # Change back to the original directory after the test
-        os.chdir(original_cwd)
+from edsl import Model
 
 
-def test_application_with_custom_env(temp_env):
-    # This test will run as if the .env is located in a new temporary directory
-    import os
-    value = os.getenv('KEY')
-    assert value == 'VALUE'
-
-    key = os.getenv('OPENAI_API_KEY')
-    assert key == "a_fake_key"
-
-    from edsl import Model
+def test_application_with_custom_env(set_env_vars):
+    # use the fixture to set  new env vars
+    # - SOME_KEY was not previously set
+    # - OPENAI_API_KEY was previously set to "a_fake_key" by pytest.ini
+    set_env_vars(OPENAI_API_KEY="a_very_fake_key", SOME_KEY="SOME_VALUE")
+    assert os.getenv("SOME_KEY") == "SOME_VALUE"
+    assert os.getenv("OPENAI_API_KEY") == "a_very_fake_key"
     m = Model()
     assert m.has_valid_api_key()
 
-    original_value = os.environ.pop('OPENAI_API_KEY', None)
+
+def test_make_sure_env_vars_were_reset():
+    # now notice that in the next test the env has been reset to its original state
+    assert os.getenv("SOME_KEY") is None
+    assert os.getenv("OPENAI_API_KEY") == "a_fake_key"
+    m = Model()
+    assert m.has_valid_api_key()
+
+
+def test_model_with_missing_key(set_env_vars):
+    # you can also use the fixture to delete an env var for the duration of the test
+    set_env_vars(OPENAI_API_KEY=None)
+    assert os.getenv("OPENAI_API_KEY") is None
+    m = Model()
     assert not m.has_valid_api_key()
-    
