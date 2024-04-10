3bde4ec5a4f51d63fbf89aefc297234b805a6a98
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-04-09 22:28:26 -0400

    formatted files

--- edsl/language_models/model_interfaces/ClaudeOpus.py
+++ edsl/language_models/model_interfaces/ClaudeOpus.py
@@ -4,12 +4,12 @@ from edsl.enums import LanguageModelType
 model_name = LanguageModelType.ANTHROPIC_3_OPUS.value
 
 ClaudeOpus = create_anthropic_model(
-    model_name=model_name, 
-    model_class_name="ClaudeOpus"
+    model_name=model_name, model_class_name="ClaudeOpus"
 )
 
 if __name__ == "__main__":
     model = ClaudeOpus()
     from edsl import QuestionFreeText
+
     results = QuestionFreeText.example().by(model).run()
     results.select("answer.*").print()
