8f4506bea3634190b294ced6fe2ee44c488fb299
robin <robin@goemeritus.com>
2024-04-16 14:55:29 -0400

    Trying to get new docs theme working

--- docs/conf.py
+++ docs/conf.py
@@ -99,7 +99,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme' #"alabaster"
+html_theme = "sphinx_rtd_theme" # "alabaster"
 # html_static_path = ["_static"]
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
