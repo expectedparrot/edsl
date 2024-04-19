d7fdd0afe527e75227865077e1dc9d665e37f230
John Horton <john.joseph.horton@gmail.com>
2024-04-19 08:52:30 -0400

    Example of a rich, doc-referencing exception class

--- edsl/exceptions/general.py
+++ edsl/exceptions/general.py
@@ -1,6 +1,28 @@
+import re
+from textwrap import dedent
+
 class GeneralErrors(Exception):
-    pass
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return self.make_urls_clickable(self.message)
+
+    @staticmethod
+    def make_urls_clickable(text):
+        url_pattern = r'https?://[^\s]+'
+        urls = re.findall(url_pattern, text)
+        for url in urls:
+            clickable_url = f'\033]8;;{url}\007{url}\033]8;;\007'
+            text = text.replace(url, clickable_url)
+        return text
 
 
 class MissingAPIKeyError(GeneralErrors):
-    pass
+    def __init__(self, model_name, inference_service):
+        full_message = dedent(f"""
+        An API Key for modle `{model_name}` is missing from the .env file.
+        This key is assocaited with the inference service `{inference_service}`.
+        Please see https://docs.expectedparrot.com/en/latest/starter_tutorial.html#part-1-using-api-keys-for-llms.
+        """)
