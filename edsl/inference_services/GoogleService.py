62763d1e6859013d4ec7d6f2f4f1f4ecb5b5eb59
John Horton <john.joseph.horton@gmail.com>
2024-04-20 12:21:25 -0400

    Uh, actually adding all the new work

---
+++ edsl/inference_services/GoogleService.py
@@ -0,0 +1,72 @@
+import os
+import aiohttp
+import json
+from typing import Any
+from edsl.exceptions import MissingAPIKeyError
+from edsl.language_models.LanguageModel import LanguageModel
+from edsl.enums import LanguageModelType, InferenceServiceType
+
+from edsl.inference_services.InferenceServiceABC import InferenceServiceABC
+
+class GoogleService(InferenceServiceABC):
+
+    _inference_service_ = "google"  
+
+    @classmethod
+    def available(cls):
+        return ['gemini-pro']
+    
+    @classmethod
+    def create_model(cls, model_name:str = "gemini-pro", model_class_name = None) -> LanguageModel:
+
+        if model_class_name is None:
+            model_class_name = cls.to_class_name(model_name)
+
+        class LLM(LanguageModel):
+
+            _model_ = model_name
+            _inference_service_ = cls._inference_service_
+            _parameters_ = {
+                "temperature": 0.5,
+                "topP": 1,
+                "topK": 1,
+                "maxOutputTokens": 2048,
+                "stopSequences": []
+            }
+
+            async def async_execute_model_call(
+                self, user_prompt: str, system_prompt: str = ""
+            ) -> dict[str, Any]:
+                #self.api_token = os.getenv("GOOGLE_API_KEY")
+                combined_prompt = user_prompt + system_prompt
+                url = f"https://generativelanguage.googleapis.com/v1beta/models/{model_name}:generateContent?key={self.api_token}"
+                headers = {"Content-Type": "application/json"}
+                data = {
+                    "contents": [{"parts": [{"text": combined_prompt}]}],
+                    "generationConfig": {
+                        "temperature": self.temperature,
+                        "topK": self.topK,
+                        "topP": self.topP,
+                        "maxOutputTokens": self.maxOutputTokens,
+                        "stopSequences": self.stopSequences,
+                    },
+                }
+
+                async with aiohttp.ClientSession() as session:
+                    async with session.post(
+                        url, headers=headers, data=json.dumps(data)
+                    ) as response:
+                        raw_response_text = await response.text()
+                        return json.loads(raw_response_text)
+
+            def parse_response(self, raw_response: dict[str, Any]) -> str:
+                data = raw_response
+                return data["candidates"][0]["content"]["parts"][0]["text"]
+            
+        LLM.__name__ = model_name
+            
+        return LLM
+
+
+if __name__ == "__main__":
+    pass
---
+++ edsl/inference_services/InferenceServiceABC.py
@@ -0,0 +1,57 @@
+from abc import abstractmethod, ABC
+from typing import Any
+import re
+
+class InferenceServiceABC(ABC):
+    """Abstract class for inference services."""
+
+    @abstractmethod
+    def available() -> list[str]:
+        pass
+
+    @abstractmethod
+    def create_model():
+        pass
+
+    @staticmethod
+    def to_class_name(s):
+        s = re.sub(r'[^a-zA-Z0-9 ]', '', s)
+        s = ''.join(word.title() for word in s.split())
+        if s and s[0].isdigit():
+            s = 'Class' + s
+        return s
+
+
+if __name__ == "__main__":
+    pass
+    #deep_infra_service = DeepInfraService("deep_infra", "DEEP_INFRA_API_KEY")
+    #deep_infra_service.available()
+    #m = deep_infra_service.create_model("microsoft/WizardLM-2-7B")
+    #response = m().hello()
+    #print(response)
+
+    #anthropic_service = AnthropicService("anthropic", "ANTHROPIC_API_KEY")
+    #anthropic_service.available()
+    #m = anthropic_service.create_model("claude-3-opus-20240229")
+    #response = m().hello()
+    #print(response)
+    # factory = OpenAIService("openai", "OPENAI_API")
+    # factory.available()
+    # m = factory.create_model("gpt-3.5-turbo")
+    #response = m().hello()
+
+    # from edsl import QuestionFreeText
+    # results = QuestionFreeText.example().by(m()).run()
+
+    # collection = InferenceServicesCollection([
+    #     OpenAIService,
+    #     AnthropicService,
+    #     DeepInfraService
+    # ])
+
+    #available = collection.available()
+    #factory = collection.create_model_factory(*available[0])
+    #m = factory()
+    #from edsl import QuestionFreeText
+    #results = QuestionFreeText.example().by(m).run()
+    #print(results)
