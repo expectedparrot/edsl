a828fec041f038b0f9b8f34ff82bad0fa71f07e5
John Horton <john.joseph.horton@gmail.com>
2024-04-20 12:16:45 -0400

    inference_service_refactor

--- edsl/enums.py
+++ edsl/enums.py
@@ -150,3 +150,9 @@ pricing = {
         completion_token_price_per_k=0.0,
     ),
 }
+
+def get_token_pricing(model_name):
+    if model_name in pricing:
+        return pricing[model_name]
+    else:
+        return TokenPricing(model_name=model_name, prompt_token_price_per_k=0.0, completion_token_price_per_k=0.0)
--- edsl/language_models/registry.py
+++ edsl/language_models/registry.py
@@ -25,11 +25,17 @@ for loader, module_name, is_pkg in pkgutil.iter_modules(model_interfaces.__path_
 meta_class_registry = RegisterLanguageModelsMeta.get_registered_classes()
 
 # For compatibility with older versions of EDSL
-get_model_class = (
+get_model_class_old = (
     lambda model_name: RegisterLanguageModelsMeta.model_names_to_classes().get(
         model_name
     )
 )
+
+def get_model_class(model_name):
+    from edsl.inference_services.services_collection import collection
+    factory = collection.create_model_factory(model_name)
+    return factory      
+
 
 
 class Meta(type):
@@ -47,32 +53,39 @@ class Meta(type):
         """
         )
 
-
 class Model(metaclass=Meta):
+
     default_model = "gpt-4-1106-preview"
 
     def __new__(cls, model_name=None, *args, **kwargs):
         # Map index to the respective subclass
-        get_model_classes = RegisterLanguageModelsMeta.model_names_to_classes()
-
-        if cls.default_model not in get_model_classes:
-            raise ValueError(f"Default model {cls.default_model} not found")
-
         if model_name is None:
             model_name = cls.default_model
-
-        subclass = get_model_classes.get(model_name, None)
-        if subclass is None:
-            raise LanguageModelNotFound(model_name)
-
-        # Create an instance of the selected subclass
-        instance = object.__new__(subclass)
-        instance.__init__(*args, **kwargs)
-        return instance
+        from edsl.inference_services.services_collection import collection
+        factory = collection.create_model_factory(model_name)
+        return factory(*args, **kwargs)
+    
+    @classmethod
+    def from_index(cls, index, *args, **kwargs):
+        from edsl.inference_services.services_collection import collection
+        model_name = collection.available()[index][0]
+        return cls(model_name, *args, **kwargs)
 
     @classmethod
-    def available(cls):
-        return sorted(list(RegisterLanguageModelsMeta.model_names_to_classes().keys()))
+    def available(cls, search_term = None, name_only = False):
+        from edsl.inference_services.services_collection import collection
+        full_list = collection.available()
+        if search_term is None:
+            if name_only:
+                return [m[0] for m in full_list]
+            else:
+                return full_list
+        else:
+            filtered_results = [m for m in full_list if search_term in m[0] or search_term in m[1]]
+            if name_only:
+                return [m[0] for m in filtered_results]
+            else:
+                return filtered_results
 
     @classmethod
     def check_models(cls, verbose=False):
