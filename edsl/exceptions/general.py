87edfd449a43bab8028d094bbf0c6adf0dfc7db8
robin <robin@goemeritus.com>
2024-05-13 07:09:59 -0400

    updated docs link for api keys instruction

--- edsl/exceptions/general.py
+++ edsl/exceptions/general.py
@@ -26,7 +26,7 @@ class MissingAPIKeyError(GeneralErrors):
             f"""
         An API Key for model `{model_name}` is missing from the .env file.
         This key is associated with the inference service `{inference_service}`.
-        Please see https://docs.expectedparrot.com/en/latest/starter_tutorial.html#part-1-using-api-keys-for-llms.
+        Please see https://docs.expectedparrot.com/en/latest/api_keys.html for more information.
         """
         )
         super().__init__(full_message)
