bd3ed41f7494b4c34bc7f8de0a81562928673e7f
John Horton <john.joseph.horton@gmail.com>
2024-05-31 09:05:30 -0400

    Improving Survey Builder

--- edsl/conjure/SurveyBuilderCSV.py
+++ edsl/conjure/SurveyBuilderCSV.py
@@ -6,7 +6,12 @@ from edsl.conjure.SurveyBuilder import SurveyBuilder
 from edsl.conjure.utilities import RCodeSnippet
 
 class SurveyBuilderCSV(SurveyBuilder):
-    
+
+
+    @staticmethod
+    def get_dataframe(datafile_name):
+        return pd.read_csv(datafile_name)
+
     def get_responses(self) -> Dict:
         """Returns a dataframe of responses by reading the datafile_name.
         
@@ -15,7 +20,7 @@ class SurveyBuilderCSV(SurveyBuilder):
 
         For example, {"Q1": [1, 2, 3], "Q2": [4, 5, 6]}
         """
-        df = pd.read_csv(self.datafile_name)
+        df = self.get_dataframe(self.datafile_name)
         df.fillna("", inplace=True)
         df = df.astype(str)
         data_dict = df.to_dict(orient="list")
@@ -26,7 +31,7 @@ class SurveyBuilderCSV(SurveyBuilder):
         Get the question name to text mapping.
         """
         d = {}
-        df = pd.read_csv(self.datafile_name)
+        df = self.get_dataframe(self.datafile_name)
         for col in df.columns:
             if col in self.lookup_dict():
                 d[col] = self.lookup_dict()[col]
@@ -43,6 +48,12 @@ class SurveyBuilderCSV(SurveyBuilder):
         d = self.get_question_name_to_text()
         return {k: {} for k, v in d.items()}
 
+
+class SurveyBuilderStata(SurveyBuilderCSV):
+
+    @staticmethod
+    def get_dataframe(datafile_name):
+        return pd.read_stata(datafile_name)
 
 if __name__ == "__main__":
     sb = SurveyBuilderCSV("responses.csv")
