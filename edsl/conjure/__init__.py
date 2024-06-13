e40f0e64c210b101785dcd65d8fcf9e80f8bb578
John Horton <john.joseph.horton@gmail.com>
2024-06-13 08:35:04 -0400

    Still working on conjure refactor

--- edsl/conjure/__init__.py
+++ edsl/conjure/__init__.py
@@ -2,3 +2,4 @@ from edsl.conjure.SurveyBuilder import SurveyBuilder
 from edsl.conjure.SurveyBuilderCSV import SurveyBuilderCSV
 from edsl.conjure.SurveyBuilderCSV import SurveyBuilderStata
 from edsl.conjure.SurveyBuilderSPSS import SurveyBuilderSPSS
+from edsl.conjure.InputData import InputData
