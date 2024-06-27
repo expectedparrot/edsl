332b4e327dadca673e5b6807cc06b8b4f9e1ed70
Reanna Ishmael <rishmael@fordham.edu>
2024-06-26 20:47:26 -0400

    Fix issue with result export test
    Since this code was not wrapped in a class or function, it was running on file save, and making unnecessary API calls.

--- tests/results/test_ResultExport.py
+++ tests/results/test_ResultExport.py
@@ -1,14 +1,23 @@
 import pytest
+import unittest
 
 from edsl.results import Results
-r = Results.example()
 
-def test_print():
-    r.print(format = "rich")
-    r.print(format = "html")
-    r.print(format = "markdown")
 
-def test_bad_name():
-    with pytest.raises(ValueError):
-        r.print(format = "bad")
+class TestResultsExport(unittest.TestCase):
 
+    def setUp(self):
+        self.r = Results.example()
+
+    def test_print(self):
+        self.r.print(format="rich")
+        self.r.print(format="html")
+        self.r.print(format="markdown")
+
+    def test_bad_name(self):
+        with pytest.raises(ValueError):
+            self.r.print(format="bad")
+
+
+if __name__ == "__main__":
+    unittest.main()
