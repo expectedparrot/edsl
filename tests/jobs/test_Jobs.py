f89020d6baa9f59049f690381293af751c297a55
John Horton <john.joseph.horton@gmail.com>
2024-07-01 10:46:22 -0400

    missing agent list dependency

--- tests/jobs/test_Jobs.py
+++ tests/jobs/test_Jobs.py
@@ -40,6 +40,7 @@ def test_jobs_simple_stuf(valid_job):
     # eval works and returns eval-able string
     assert "Jobs(survey=Survey(" in repr(valid_job)
     from edsl import ScenarioList
+    from edsl import AgentList
     assert isinstance(eval(repr(valid_job)), Jobs)
     # serialization
     assert isinstance(valid_job.to_dict(), dict)
