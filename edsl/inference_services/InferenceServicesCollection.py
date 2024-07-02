9a29cb8f4747d6f23be5ea656e73d432125456c7
John Horton <john.joseph.horton@gmail.com>
2024-07-02 09:08:13 -0400

    Need to also use cached models in other settings

--- edsl/inference_services/InferenceServicesCollection.py
+++ edsl/inference_services/InferenceServicesCollection.py
@@ -13,20 +13,22 @@ class InferenceServicesCollection:
             cls.added_models[service_name] = []
         cls.added_models[service_name].append(model_name)
 
+    @staticmethod
+    def _get_service_available(service) -> list[str]:
+        from_api = True
+        try:
+            service_models = service.available()
+        except Exception as e:
+            print(f"Error getting models for {service._inference_service_}. Relying on cache.")
+            from edsl.inference_services.models_available_cache import models_available
+            service_models = models_available.get(service._inference_service_, [])
+            from_api = False
+        return service_models #, from_api
+
     def available(self):
-        msg = "See instructions about storing your API keys: https://docs.expectedparrot.com/en/latest/api_keys.html"
-        msg_printed = False
         total_models = []
         for service in self.services:
-            try:
-                service_models = service.available()
-            except Exception as e:
-                if not msg_printed:
-                    print(msg)
-                    msg_printed = True
-                print(f"Error getting models for {service._inference_service_}. Relying on cache.")
-                from edsl.inference_services.models_available_cache import models_available
-                service_models = models_available.get(service._inference_service_, [])
+            service_models = self._get_service_available(service)
             for model in service_models:
                 total_models.append([model, service._inference_service_, -1])
 
@@ -44,7 +46,7 @@ class InferenceServicesCollection:
 
     def create_model_factory(self, model_name: str, service_name=None, index=None):
         for service in self.services:
-            if model_name in service.available():
+            if model_name in self._get_service_available(service):
                 if service_name is None or service_name == service._inference_service_:
                     return service.create_model(model_name)
 
