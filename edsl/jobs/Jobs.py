106ee6b63d58618158a19c865822eba7067ce4c3
Reanna Ishmael <rishmael@fordham.edu>
2024-08-04 16:00:22 -0400

    Fix remote inference results

--- edsl/jobs/Jobs.py
+++ edsl/jobs/Jobs.py
@@ -461,12 +461,11 @@ class Jobs(Base):
             remote_inference = False
 
         if remote_inference:
-            from edsl.agents.Agent import Agent
-            from edsl.language_models.registry import Model
-            from edsl.results.Result import Result
-            from edsl.results.Results import Results
-            from edsl.scenarios.Scenario import Scenario
-            from edsl.surveys.Survey import Survey
+            import time
+            from datetime import datetime
+            from edsl.config import CONFIG
+
+            expected_parrot_url = CONFIG.get("EXPECTED_PARROT_URL")
 
             self._output("Remote inference activated. Sending job to server...")
             if remote_cache:
@@ -474,31 +473,53 @@ class Jobs(Base):
                     "Remote caching activated. The remote cache will be used for this job."
                 )
 
-            remote_job_data = coop.remote_inference_create(
+            remote_job_creation_data = coop.remote_inference_create(
                 self,
                 description=remote_inference_description,
                 status="queued",
             )
-            self._output("Job sent!")
-            # Create mock results object to store job data
-            results = Results(
-                survey=Survey(),
-                data=[
-                    Result(
-                        agent=Agent.example(),
-                        scenario=Scenario.example(),
-                        model=Model(),
-                        iteration=1,
-                        answer={"info": "Remote job details"},
+            time_queued = datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
+            job_uuid = remote_job_creation_data.get("uuid")
+            print(f"Remote inference job with uuid={job_uuid}.")
+            print(f"Job queued at {time_queued}.")
+            job_in_queue = True
+            while job_in_queue:
+                remote_job_data = coop.remote_inference_get(job_uuid)
+                time_checked = datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
+                status = remote_job_data.get("status")
+                if status == "cancelled":
+                    print("\nJob was cancelled by user.")
+                    print(
+                        f"Please see {expected_parrot_url}/home/remote-inference for more details."
                     )
-                ],
-            )
-            results.add_columns_from_dict([remote_job_data])
-            if self.verbose:
-                results.select(["info", "uuid", "status", "version"]).print(
-                    format="rich"
-                )
-            return results
+                    return None
+                elif status == "failed":
+                    print("\nJob failed.")
+                    print(
+                        f"Please see {expected_parrot_url}/home/remote-inference for more details."
+                    )
+                    return None
+                elif status == "completed":
+                    print("\nJob finished successfully!")
+                    results_uuid = remote_job_data.get("results_uuid")
+                    print(f"Results with uuid={results_uuid} created.")
+
+                    # Get results from server
+                    results = coop.get(results_uuid, expected_object_type="results")
+                    return results
+                # Status is either queued, running, or cancelling, so keep checking
+                else:
+                    print(
+                        f"\rJob checked at {time_checked}. Status: {status}.",
+                        end="",
+                        flush=True,
+                    )
+                    if len(self) < 10:
+                        # Check once every 10 seconds for small jobs
+                        time.sleep(10)
+                    else:
+                        # Check once every minute for larger jobs
+                        time.sleep(60)
         else:
             if check_api_keys:
                 for model in self.models + [Model()]:
