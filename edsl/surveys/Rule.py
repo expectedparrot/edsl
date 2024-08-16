eaf00d56e4b1bad8d2a1aa49b6706ba84193a993
John Horton <john.joseph.horton@gmail.com>
2024-08-16 11:32:04 -0400

    More hash futzing

--- edsl/surveys/Rule.py
+++ edsl/surveys/Rule.py
@@ -117,11 +117,13 @@ class Rule:
     def _checks(self):
         pass
 
-    @add_edsl_version
+    # def _to_dict(self):
+
+    # @add_edsl_version
     def to_dict(self):
         """Convert the rule to a dictionary for serialization.
 
-        >>> r = Rule.example()
+        >>> r = Rule.example(
         >>> r.to_dict()
         {'current_q': 1, 'expression': "q1 == 'yes'", 'next_q': 2, 'priority': 0, 'question_name_to_index': {'q1': 1}, 'before_rule': False, 'edsl_version': '...', 'edsl_class_name': 'Rule'}
         """
@@ -133,6 +135,7 @@ class Rule:
             "question_name_to_index": self.question_name_to_index,
             "before_rule": self.before_rule,
         }
+        # return self._to_dict()
 
     @classmethod
     @remove_edsl_version
