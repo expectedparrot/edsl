e0ac49d96782aace13971f079e017f6373750def
John Horton <john.joseph.horton@gmail.com>
2024-08-27 13:33:31 -0400

    Fix config var in Study

--- edsl/study/Study.py
+++ edsl/study/Study.py
@@ -236,7 +236,7 @@ class Study:
         if self.verbose:
             print("Existing objects in study:")
             self.print()
-        snapshot = SnapShot(self.namespace, exclude=[self, Config])
+        snapshot = SnapShot(self.namespace, exclude=[self])
         if self.use_study_cache:
             if self.verbose:
                 print("Using study cache.")
