358838d71daaf754ceb924e5e67641db642d44af
John Horton <john.joseph.horton@gmail.com>
2024-09-15 19:44:49 -0400

    Only doing MC

--- tests/questions/test_examples.py
+++ tests/questions/test_examples.py
@@ -4,7 +4,8 @@ from edsl import Question
 
 @pytest.mark.parametrize("question_type", Question.available())
 def test_individual_questions(question_type):
-    if question_type != "functional" and question_type != "extract":
+    # if question_type != "functional" and question_type != "extract":
+    if question_type == "multiple_choice":
         q = Question.example(question_type)
         r = q.example_results()
         _ = hash(r)
