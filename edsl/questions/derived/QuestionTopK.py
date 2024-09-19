60196d55953f167843917f1b437d6acabe020e7c
John Horton <john.joseph.horton@gmail.com>
2024-09-19 13:26:08 -0500

    Add 'include_comment' field

--- edsl/questions/derived/QuestionTopK.py
+++ edsl/questions/derived/QuestionTopK.py
@@ -20,6 +20,7 @@ class QuestionTopK(QuestionCheckBox):
         max_selections: int,
         question_presentation: Optional[str] = None,
         answering_instructions: Optional[str] = None,
+        include_comment: Optional[bool] = True,
     ):
         """Initialize the question.
 
@@ -37,6 +38,7 @@ class QuestionTopK(QuestionCheckBox):
             max_selections=max_selections,
             question_presentation=question_presentation,
             answering_instructions=answering_instructions,
+            include_comment=include_comment,
         )
         if min_selections != max_selections:
             raise QuestionCreationValidationError(
