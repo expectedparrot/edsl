92afba8d2847a500751d4bc6a741580f0ebe6c2c
Apostolos Filippas <apostolosfilippas@gmail.com>
2024-09-20 14:08:57 -0400

    formatting

--- edsl/questions/Quick.py
+++ edsl/questions/Quick.py
@@ -1,20 +1,32 @@
-from edsl import QuestionFreeText, QuestionMultipleChoice, Survey, QuestionList, Question
+from edsl import (
+    QuestionFreeText,
+    QuestionMultipleChoice,
+    Survey,
+    QuestionList,
+    Question,
+)
 
-def Quick(question_text): 
 
+def Quick(question_text):
     q_type = QuestionMultipleChoice(
-        question_text = f"A researcher is asking a language model this: {question_text}. What is the most appropriate type of question to ask?", 
-        question_name = "potential_question_type",
-        question_options = ["multiple_choice", "list", "free_text"])
-    
+        question_text=f"A researcher is asking a language model this: {question_text}. What is the most appropriate type of question to ask?",
+        question_name="potential_question_type",
+        question_options=["multiple_choice", "list", "free_text"],
+    )
+
     q_name = QuestionFreeText(
-        question_text = f"A researcher is asking a language model this: {question_text}. What is a good name for this question that's a valid python identifier? Just return the proposed identifer", 
-        question_name = "potential_question_name")
-    
-    q_options = QuestionList(question_text = f"A research is asking this question: { question_text }. What are the possible options for this question?", 
-                             question_name = "potential_question_options")
-    
-    survey = Survey([q_type, q_name, q_options]).add_skip_rule(q_options, "{{ potential_question_type }} != 'multiple_choice'")
+        question_text=f"A researcher is asking a language model this: {question_text}. What is a good name for this question that's a valid python identifier? Just return the proposed identifer",
+        question_name="potential_question_name",
+    )
+
+    q_options = QuestionList(
+        question_text=f"A research is asking this question: { question_text }. What are the possible options for this question?",
+        question_name="potential_question_options",
+    )
+
+    survey = Survey([q_type, q_name, q_options]).add_skip_rule(
+        q_options, "{{ potential_question_type }} != 'multiple_choice'"
+    )
     return survey
     # results = survey.run()
     # question_type = results.select("potential_question_type").first()
@@ -27,5 +39,3 @@ def Quick(question_text):
     #     return Question(question_type, question_name = question_name)
     # else:
     #     return Question(question_type, question_name = question_name, question_options = question_options)
-
-    
