29ee8f8313d8fb0edeca42f6097bd408bcde68fe
John Horton <john.joseph.horton@gmail.com>
2024-10-04 08:21:00 -0400

    Refactoring prompt constructor for clarity

--- edsl/agents/PromptConstructor.py
+++ edsl/agents/PromptConstructor.py
@@ -10,16 +10,7 @@ from edsl.data_transfer_models import ImageInfo
 from edsl.prompts.registry import get_classes as prompt_lookup
 from edsl.exceptions import QuestionScenarioRenderError
 
-# from edsl import Agent
-
-# from edsl.scenarios.FileStore import FileStore
-
-
-class PromptComponent(enum.Enum):
-    AGENT_INSTRUCTIONS = "agent_instructions"
-    AGENT_PERSONA = "agent_persona"
-    QUESTION_INSTRUCTIONS = "question_instructions"
-    PRIOR_QUESTION_MEMORY = "prior_question_memory"
+from edsl.agents.prompt_helpers import PromptComponent, PromptList, PromptPlan
 
 
 def get_jinja2_variables(template_str: str) -> Set[str]:
@@ -35,124 +26,6 @@ def get_jinja2_variables(template_str: str) -> Set[str]:
     env = Environment()
     ast = env.parse(template_str)
     return meta.find_undeclared_variables(ast)
-
-
-class PromptList(UserList):
-    separator = Prompt(" ")
-
-    def reduce(self):
-        """Reduce the list of prompts to a single prompt.
-
-        >>> p = PromptList([Prompt("You are a happy-go lucky agent."), Prompt("You are an agent with the following persona: {'age': 22, 'hair': 'brown', 'height': 5.5}")])
-        >>> p.reduce()
-        Prompt(text=\"""You are a happy-go lucky agent. You are an agent with the following persona: {'age': 22, 'hair': 'brown', 'height': 5.5}\""")
-
-        """
-        p = self[0]
-        for prompt in self[1:]:
-            if len(prompt) > 0:
-                p = p + self.separator + prompt
-        return p
-
-
-class PromptPlan:
-    """A plan for constructing prompts for the LLM call.
-    Every prompt plan has a user prompt order and a system prompt order.
-    It must contain each of the values in the PromptComponent enum.
-
-
-    >>> p = PromptPlan(user_prompt_order=(PromptComponent.AGENT_INSTRUCTIONS, PromptComponent.AGENT_PERSONA),system_prompt_order=(PromptComponent.QUESTION_INSTRUCTIONS, PromptComponent.PRIOR_QUESTION_MEMORY))
-    >>> p._is_valid_plan()
-    True
-
-    >>> p.arrange_components(agent_instructions=1, agent_persona=2, question_instructions=3, prior_question_memory=4)
-    {'user_prompt': ..., 'system_prompt': ...}
-
-    >>> p = PromptPlan(user_prompt_order=("agent_instructions", ), system_prompt_order=("question_instructions", "prior_question_memory"))
-    Traceback (most recent call last):
-    ...
-    ValueError: Invalid plan: must contain each value of PromptComponent exactly once.
-
-    """
-
-    def __init__(
-        self,
-        user_prompt_order: Optional[tuple] = None,
-        system_prompt_order: Optional[tuple] = None,
-    ):
-        """Initialize the PromptPlan."""
-
-        if user_prompt_order is None:
-            user_prompt_order = (
-                PromptComponent.QUESTION_INSTRUCTIONS,
-                PromptComponent.PRIOR_QUESTION_MEMORY,
-            )
-        if system_prompt_order is None:
-            system_prompt_order = (
-                PromptComponent.AGENT_INSTRUCTIONS,
-                PromptComponent.AGENT_PERSONA,
-            )
-
-        # very commmon way to screw this up given how python treats single strings as iterables
-        if isinstance(user_prompt_order, str):
-            user_prompt_order = (user_prompt_order,)
-
-        if isinstance(system_prompt_order, str):
-            system_prompt_order = (system_prompt_order,)
-
-        if not isinstance(user_prompt_order, tuple):
-            raise TypeError(
-                f"Expected a tuple, but got {type(user_prompt_order).__name__}"
-            )
-
-        if not isinstance(system_prompt_order, tuple):
-            raise TypeError(
-                f"Expected a tuple, but got {type(system_prompt_order).__name__}"
-            )
-
-        self.user_prompt_order = self._convert_to_enum(user_prompt_order)
-        self.system_prompt_order = self._convert_to_enum(system_prompt_order)
-        if not self._is_valid_plan():
-            raise ValueError(
-                "Invalid plan: must contain each value of PromptComponent exactly once."
-            )
-
-    def _convert_to_enum(self, prompt_order: tuple):
-        """Convert string names to PromptComponent enum values."""
-        return tuple(
-            PromptComponent(component) if isinstance(component, str) else component
-            for component in prompt_order
-        )
-
-    def _is_valid_plan(self):
-        """Check if the plan is valid."""
-        combined = self.user_prompt_order + self.system_prompt_order
-        return set(combined) == set(PromptComponent)
-
-    def arrange_components(self, **kwargs) -> Dict[PromptComponent, Prompt]:
-        """Arrange the components in the order specified by the plan."""
-        # check is valid components passed
-        component_strings = set([pc.value for pc in PromptComponent])
-        if not set(kwargs.keys()) == component_strings:
-            raise ValueError(
-                f"Invalid components passed: {set(kwargs.keys())} but expected {PromptComponent}"
-            )
-
-        user_prompt = PromptList(
-            [kwargs[component.value] for component in self.user_prompt_order]
-        )
-        system_prompt = PromptList(
-            [kwargs[component.value] for component in self.system_prompt_order]
-        )
-        return {"user_prompt": user_prompt, "system_prompt": system_prompt}
-
-    def get_prompts(self, **kwargs) -> Dict[str, Prompt]:
-        """Get both prompts for the LLM call."""
-        prompts = self.arrange_components(**kwargs)
-        return {
-            "user_prompt": prompts["user_prompt"].reduce(),
-            "system_prompt": prompts["system_prompt"].reduce(),
-        }
 
 
 class PromptConstructor:
@@ -175,12 +48,10 @@ class PromptConstructor:
         self.model = invigilator.model
         self.current_answers = invigilator.current_answers
         self.memory_plan = invigilator.memory_plan
-        self.prompt_plan = PromptPlan()  # Assuming PromptPlan is defined elsewhere
-
-        # prompt_plan = PromptPlan()
+        self.prompt_plan = PromptPlan()
 
     @property
-    def scenario_file_keys(self):
+    def scenario_file_keys(self) -> list:
         """We need to find all the keys in the scenario that refer to FileStore objects.
         These will be used to append to the prompt a list of files that are part of the scenario.
         """
@@ -270,14 +141,6 @@ class PromptConstructor:
 
     def prior_answers_dict(self) -> dict:
         d = self.survey.question_names_to_questions()
-
-        # Every prior question should be answered
-        # parent_questions_that_should_be_answered = self.survey.dag(textify=True).get(
-        #     self.question.question_name, []
-        # )
-        # for parent_question in parent_questions_that_should_be_answered:
-        #     assert parent_question in self.current_answers
-
         # This attaches the answer to the question
         for question, answer in self.current_answers.items():
             if question in d:
