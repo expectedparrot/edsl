402bd944db3b7cfeffa8379494a8d6c7192c56ff
John Horton <john.joseph.horton@gmail.com>
2024-12-11 20:59:46 -0500

    Export to PDF option for text

--- edsl/results/Dataset.py
+++ edsl/results/Dataset.py
@@ -13,6 +13,55 @@ from edsl.results.TableDisplay import TableDisplay
 from edsl.Base import PersistenceMixin, HashingMixin
 
 
+class SmartInt(int):
+    pass
+
+
+class SmartFloat(float):
+    pass
+
+
+class SmartStr(str):
+
+    def write(self, filename: str):
+        with open(filename, "w") as f:
+            f.write(str(self))
+        return None
+
+    def _repr_html_(self):
+        pass
+
+    def markdown(self):
+        return SmartMarkdown(self)
+
+    def pdf(self, filename: Optional[str] = None):
+        # renders the markdown as a pdf that can be downloaded
+        from edsl.results.MarkdownToPDF import MarkdownToPDF
+
+        return MarkdownToPDF(self, filename).preview()
+
+
+class SmartMarkdown(SmartStr):
+    def _repr_markdown_(self):
+        return self
+
+    def _repr_html_(self):
+        from IPython.display import Markdown, display
+
+        display(Markdown(self))
+
+
+class FirstObject:
+    def __new__(self, value):
+        if isinstance(value, int):
+            return SmartInt(value)
+        if isinstance(value, float):
+            return SmartFloat(value)
+        if isinstance(value, str):
+            return SmartStr(value)
+        return value
+
+
 class Dataset(UserList, ResultsExportMixin, PersistenceMixin, HashingMixin):
     """A class to represent a dataset of observations."""
 
@@ -210,7 +259,7 @@ class Dataset(UserList, ResultsExportMixin, PersistenceMixin, HashingMixin):
             """Get the values of the first key in the dictionary."""
             return list(d.values())[0]
 
-        return get_values(self.data[0])[0]
+        return FirstObject(get_values(self.data[0])[0])
 
     def remove_prefix(self) -> Dataset:
         new_data = []
