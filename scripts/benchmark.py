4eb7d1ad24a9ba92354ecd04c48a21aa93b2e5cb
zer0dss <mudragstefan@gmail.com>
2024-12-22 17:51:01 +0200

    add benchmark script for remote inference test

---
+++ scripts/benchmark.py
@@ -0,0 +1,58 @@
+from edsl import Jobs
+import requests 
+import json
+from edsl import QuestionCheckBox,QuestionFreeText,QuestionList
+import time
+import threading
+from concurrent.futures import ThreadPoolExecutor
+import matplotlib
+from edsl import Survey,Results
+from edsl import Cache
+import matplotlib.pyplot as plt
+
+
+running_time = {}
+def test_1(disable_remote_inference=False,th_id=0):
+
+    start = time.time()
+    try:
+        q = QuestionCheckBox.example()
+        cache = Cache()
+
+        res = Survey(questions=[q]).run(disable_remote_inference=disable_remote_inference,disable_remote_cache=True,cache=cache)
+
+    except Exception as e:
+        print(e)    
+    end = time.time()
+    print("Time taken to run the survey: ", end-start)
+    running_time[th_id] = end-start
+
+def run_tests_in_parallel(disable_remote_inference=False,nr_jobs=2):
+    with ThreadPoolExecutor(max_workers=20) as executor:
+        futures = [executor.submit(test_1,disable_remote_inference=disable_remote_inference,th_id=i) for i in range(nr_jobs)]
+        for future in futures:
+            future.result()
+def plot_times(disable_remote_inference=False):
+    fig = plt.figure()
+
+    values = [running_time[k] for k in range(len(running_time))]
+    max_value = max(values)
+    plt.plot(values)
+    plt.ylim(0, max(max_value, 12))
+    plt.xlabel('Job Number')
+    plt.ylabel('Execution Time (s)')
+    plt.title('Execution Time per Job')
+    plt.xticks(range(len(running_time)))  # Set x-axis to display only integer positions
+    if disable_remote_inference:
+        plt.savefig(f'plot_{len(running_time)}_no_remote.png')
+    else:
+        plt.savefig(f'plot_{len(running_time)}.png')
+    plt.show()
+
+####################
+# Main
+disable_remote_inference=True
+
+run_tests_in_parallel(disable_remote_inference=disable_remote_inference, nr_jobs=100)
+
+plot_times(disable_remote_inference=disable_remote_inference)
