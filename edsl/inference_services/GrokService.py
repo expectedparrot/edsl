25fbf679518040a90b66baf96157fc6bbae94ab4
zer0dss <mudragstefan@gmail.com>
2025-02-14 12:43:38 +0200

    add grok service

--- edsl/inference_services/GrokService.py
+++ edsl/inference_services/GrokService.py
@@ -6,6 +6,6 @@ class GrokService(OpenAIService):
     """Openai service class."""
 
     _inference_service_ = "grok"
-    _env_key_name_ = "XAI_API_KEY"
+    _env_key_name_ = "GROQ_API_KEY"
     _base_url_ = "https://api.x.ai/v1"
     _models_list_cache: List[str] = []
