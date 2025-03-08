80fdea9fdff83f426c3617e8516260a7aa688ad2
John Horton <john.joseph.horton@gmail.com>
2025-03-08 12:17:25 -0500

    Refactor survey folder

--- edsl/surveys/__init__.py
+++ edsl/surveys/__init__.py
@@ -1,5 +1,5 @@
-from edsl.surveys.Survey import Survey
-from edsl.surveys.instructions.Instruction import Instruction
+from .survey import Survey
+from .instructions.Instruction import Instruction
 
 # from edsl.surveys.Rule import Rule
 # from edsl.surveys.RuleCollection import RuleCollection
