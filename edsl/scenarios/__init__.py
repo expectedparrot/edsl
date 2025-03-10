2c2a2a8bcc181435d96e82c5612e26f08f8f5ce7
John Horton <john.joseph.horton@gmail.com>
2025-03-09 21:31:57 -0400

    Getting exports right

--- edsl/scenarios/__init__.py
+++ edsl/scenarios/__init__.py
@@ -1,3 +1,5 @@
-from edsl.scenarios.scenario import Scenario
-from edsl.scenarios.scenario_list import ScenarioList
-from edsl.scenarios.file_store import FileStore
+from .scenario import Scenario
+from .scenario_list import ScenarioList
+from .file_store import FileStore
+
+__all__ = ["Scenario", "ScenarioList", "FileStore"]
