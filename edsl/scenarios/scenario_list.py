c1e360f98ff6bb2d5acf25b3fcf36be4327eec1c
John Horton <john.joseph.horton@gmail.com>
2025-04-07 07:53:42 -0400

    Fix bug in dataset & add some more tests

--- edsl/scenarios/scenario_list.py
+++ edsl/scenarios/scenario_list.py
@@ -1992,7 +1992,7 @@ class ScenarioList(MutableSequence, Base, ScenarioListOperationsMixin):
         for key, values in grouped.items():
             new_scenario = dict(zip(id_vars, key))
             if separator:
-                new_scenario[field] = separator.join(values)
+                new_scenario[field] = separator.join([str(x) for x in values])
             else:
                 new_scenario[field] = values
             if add_count:
