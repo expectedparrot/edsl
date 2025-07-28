36d491f9151aa4aa65c1c8ba3c9b3318ff5160a0
John Horton <john.joseph.horton@gmail.com>
2025-07-27 20:58:33 -0400

    Update to use widgets

--- edsl/agents/agent_list.py
+++ edsl/agents/agent_list.py
@@ -129,6 +129,10 @@ class AgentList(UserList, Base, AgentListOperationsMixin):
     def manage(cls):
         from ..widgets.agent_list_manager import AgentListManagerWidget
         return AgentListManagerWidget()
+    
+    def edit(self):
+        from ..widgets.agent_list_builder import AgentListBuilderWidget
+        return AgentListBuilderWidget(self)
 
     def set_traits_presentation_template(
         self, traits_presentation_template: str
