41df12ea801ad7f9fae0d265377fd36710bd8ff1
John Horton <john.joseph.horton@gmail.com>
2025-07-28 06:11:36 -0400

    New widgets

--- edsl/widgets/__init__.py
+++ edsl/widgets/__init__.py
@@ -7,9 +7,11 @@ This package contains interactive widgets for visualizing and working with EDSL 
 from .base_widget import EDSLBaseWidget
 from .results_viewer import ResultsViewerWidget
 from .agent_list_manager import AgentListManagerWidget
+from .object_docs_viewer import ObjectDocsViewerWidget
 
 __all__ = [
     'EDSLBaseWidget',
     'ResultsViewerWidget',
     'AgentListManagerWidget',
+    'ObjectDocsViewerWidget',
 ] 
