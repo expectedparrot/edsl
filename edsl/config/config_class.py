355fc8b77e7b2ba92682a600f15855d15cdb9015
Rae <rishmael@fordham.edu>
2025-07-31 09:54:30 -0400

    Remove unused widget scenario UUID config var

--- edsl/config/config_class.py
+++ edsl/config/config_class.py
@@ -87,10 +87,6 @@ CONFIG_MAP = {
         "default": "https://www.expectedparrot.com",
         "info": "This config var holds the URL of the Expected Parrot API.",
     },
-    "EDSL_WIDGET_SCENARIO_UUID": {
-        "default": "",
-        "info": "This config var holds the UUID of the scenario list containing widget assets (ESM and CSS files).",
-    },
     "EDSL_MAX_CONCURRENT_TASKS": {
         "default": "1000",
         "info": "This config var determines the maximum number of concurrent tasks that can be run by the async job-runner",
