d986277c6e4add8c15405d188e10f0e870019f96
zer0dss <mudragstefan@gmail.com>
2025-10-15 12:16:44 +0000

    debug api keys used

--- edsl/scenarios/file_store.py
+++ edsl/scenarios/file_store.py
@@ -173,7 +173,12 @@ class FileStore(Scenario):
             from ..coop import Coop
 
             coop = Coop()
-            print("[DEBUG_key]", coop.api_key, coop.api_url)
+            print(
+                "[DEBUG_key]",
+                coop.api_key,
+                coop.api_url,
+                os.environ["EXPECTED_PARROT_API_KEY"],
+            )
             response = coop._send_server_request(
                 uri="api/v0/filestore/download-url",
                 method="POST",
