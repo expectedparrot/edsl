e55390829c9f8583eccb083ab0d14f7497d25e69
John Horton <john.joseph.horton@gmail.com>
2025-10-28 06:39:04 -0700

    Prepares a report showing differences between the answers of two result objects

--- edsl/comparisons/result_differences.py
+++ edsl/comparisons/result_differences.py
@@ -10,12 +10,26 @@ if TYPE_CHECKING:
 from ..prompts import Prompt
 
 base_template = Prompt(text = """
-An AI agent was asked the question "{{ question_text }}"
-The question type was "{{ question_type }}".
-The question options were "{{ question_options }}"
+<question>
+{{ question_text }}
+</question>
 
-The true answer from the human was: {{ actual_answer }}
-The AI agent's answer was: "{{ candidate_answer }}"
+<question_type>
+{{ question_type }}
+</question_type>
+
+The question options were 
+<question_options>
+{{ question_options }}"
+</question_options>
+
+<actual_answer>
+{{ actual_answer }}
+</actual_answer>
+
+<candidate_answer>
+{{ candidate_answer }}
+</candidate_answer>
 """)
 
 class ResultDifferences:
@@ -27,6 +41,7 @@ class ResultDifferences:
         result_pair_comparison: "ResultPairComparison",
         template: Optional[str] = None,
         exclude_exact_match: bool = True,
+        separator = "\n\n--------------------------------\n"
     ):
         """Initialize ResultDifferences container.
 
@@ -43,6 +58,7 @@ class ResultDifferences:
             template = Prompt(text = template)
         self.template = template
         self.exclude_exact_match = exclude_exact_match
+        self.separator = separator
 
     @classmethod
     def example(cls) -> ResultDifferences:
@@ -53,16 +69,16 @@ class ResultDifferences:
     def generate_report(self) -> str:
         """Generate a report on the differences between the two results."""
         report = Prompt("")
-        for scenario in self.result_pair_comparison.to_scenario_list():
+        for question_name, comparison_data in self.result_pair_comparison.comparisons.items():
             d = {}
-            d['question_text'] = scenario.question_text
-            d['question_type'] = scenario.question_type
-            d['question_options'] = scenario.question_options
-            d['actual_answer'] = scenario.answer_b
-            d['candidate_answer'] = scenario.answer_a
+            d['question_text'] = comparison_data['question_info']['question_text']
+            d['question_type'] = comparison_data['question_info']['question_type']
+            d['question_options'] = comparison_data['question_info']['question_options']
+            d['actual_answer'] = comparison_data['answer_b']
+            d['candidate_answer'] = comparison_data['answer_a']
             for key, value in self.result_pair_comparison.scenario_A.items():
                 d[key] = value
-            report += self.template.render(primary_replacement=d)
+            report += self.template.render(primary_replacement=d) + self.separator
         return report  
 
 
