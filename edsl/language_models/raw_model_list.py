3e2cebcdcf1f491ea7d1952466aa7cbe66f7dcde
John Horton <john.joseph.horton@gmail.com>
2026-01-10 17:59:33 -0500

    Missing file

---
+++ edsl/language_models/raw_model_list.py
@@ -0,0 +1,31 @@
+"""
+RawModelList - A simple list of Models without event-sourcing.
+
+This is used for testing scenarios where Models have custom behavior
+(like func=, throw_exception, scripted_responses) that cannot be serialized.
+
+For production use, prefer ModelList which has git-like versioning support.
+"""
+
+from __future__ import annotations
+from typing import Optional, List, TYPE_CHECKING, Any
+from collections import UserList
+
+if TYPE_CHECKING:
+    from ..language_models import LanguageModel
+
+
+class RawModelList(UserList):
+    """
+    A simple list of Model objects without event-sourcing.
+
+    Used for tests that need Model features that can't be serialized:
+    - Custom func= parameter
+    - throw_exception / exception_probability
+    - from_scripted_responses()
+
+    For production code, use ModelList instead.
+    """
+
+    def __init__(self, data: Optional[List["LanguageModel"]] = None):
+        super().__init__(data or [])
