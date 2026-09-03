d6b18e0d6494dce0f879943f39386533f5244adb
John Horton <john.joseph.horton@gmail.com>
2026-09-03 05:58:14 -0400

    Add serializable causal study blueprints

--- edsl/causal/__init__.py
+++ edsl/causal/__init__.py
@@ -8,9 +8,11 @@ from .edsl_adapter import AdapterCall, EDSLCausalAdapter
 from .model import Equation, StructuralCausalModel
 from .runner import CausalExperimentRunner, CausalObservation, ConversationTurnRequest, MeasurementRequest
 from .variables import EndogenousVariable, ExogenousVariable, Measurement, ParticipantScope, ScenarioScope
+from .blueprint import BlueprintCompiler, BlueprintValidation, CausalStudyBlueprint, CompiledCausalStudy, DesignPolicy, ExecutionChannel, InformationPolicy, ProcedureRequirement, ResearchQuestion, StudyRole, ValidationFinding
 
 __all__ = [
     "AdapterCall", "AgentRole", "CausalAnalysisPlan", "CausalExperimentRunner", "CausalObservation", "CompiledExperiment", "CompiledReplication", "ConversationTurnRequest", "EDSLCausalAdapter", "EndogenousVariable", "Equation", "EstimatorSpec", "ExperimentCompiler",
     "ExperimentDesign", "ExogenousVariable", "FittedEquation", "FittedSCM", "Measurement", "ParticipantScope",
     "MeasurementManifest", "MeasurementRequest", "ParticipantAssignment", "PathEffect", "ScenarioScope", "StructuralCausalModel", "TreatmentCell",
+    "BlueprintCompiler", "BlueprintValidation", "CausalStudyBlueprint", "CompiledCausalStudy", "DesignPolicy", "ExecutionChannel", "InformationPolicy", "ProcedureRequirement", "ResearchQuestion", "StudyRole", "ValidationFinding",
 ]
